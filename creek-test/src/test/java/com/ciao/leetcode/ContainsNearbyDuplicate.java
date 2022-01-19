b5b03e9cdb34b4203367b278c6e65bd9c294be41
caochengyin <caochengyin@jd.com>
2022-01-19 17:10:15 +0800

    leet code commit 2022/1/19

---
+++ creek-test/src/test/java/com/ciao/leetcode/ContainsNearbyDuplicate.java
@@ -0,0 +1,35 @@
+package com.ciao.leetcode;
+
+import java.util.HashSet;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/1/19
+ * @description https://leetcode-cn.com/problems/contains-duplicate-ii/
+ */
+public class ContainsNearbyDuplicate {
+    public static void main(String[] args) {
+        boolean b = new ContainsNearbyDuplicate().containsNearbyDuplicate(new int[]{1, 0, 1, 1}, 1);
+        System.out.println(b);
+    }
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums == null || nums.length == 1) {
+            return false;
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+            }
+
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ creek-test/src/test/java/com/ciao/leetcode/SearchInsert.java
@@ -0,0 +1,45 @@
+package com.ciao.leetcode;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/1/17
+ * @description https://leetcode-cn.com/problems/search-insert-position/
+ */
+public class SearchInsert {
+    public static void main(String[] args) {
+        SearchInsert insert = new SearchInsert();
+        System.out.println(insert.searchInsert(new int[]{1,3,5,6},7));
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        int length = nums.length;
+        int left = 0,right = length-1;
+
+        if (target <= nums[left]){
+            return left;
+
+        }else if (target == nums[right]){
+            return right;
+
+        }else if (target > nums[right]){
+            return right+1;
+        }
+
+        while (left < right){
+            int mid = left + (right-left)/2;
+            int num = nums[mid];
+            if (target == num){
+                return mid;
+
+            }else if (target > num){
+                left = mid+1;
+
+            }else{
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+}
