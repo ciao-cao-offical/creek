042d8c79d4a54f7cdeb17720cf0afa08aa6456f6
caochengyin <caochengyin@jd.com>
2022-01-23 18:16:45 +0800

    LeetCode commit 2022/1/23

---
+++ creek-test/src/test/java/com/ciao/leetcode/Connect.java
@@ -0,0 +1,63 @@
+package com.ciao.leetcode;
+
+import java.util.LinkedList;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/1/23
+ * @description https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/
+ */
+public class Connect {
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+
+        LinkedList<Node> nodes = new LinkedList<>();
+        nodes.offer(root);
+
+        while (!nodes.isEmpty()) {
+            int size = nodes.size();
+            for (int i = 0; i < size; i++) {
+                Node poll = nodes.poll();
+                if (i < size - 1) {
+                    poll.next = nodes.peek();
+                }
+
+                if (poll.left != null) {
+                    nodes.offer(poll.left);
+                }
+
+                if (poll.right != null) {
+                    nodes.offer(poll.right);
+                }
+            }
+        }
+
+        return root;
+    }
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    ;
+}
