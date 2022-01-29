0d4095fe200c4e2f046b17fed51ce682f5f8f643
caochengyin <caochengyin@jd.com>
2022-01-29 15:04:49 +0800

    LeetCode commit 2022/1/29

---
+++ creek-test/src/test/java/com/ciao/leetcode/SingleNumber.java
@@ -0,0 +1,17 @@
+package com.ciao.leetcode;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/1/29
+ * @description https://leetcode-cn.com/problems/single-number/
+ */
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int s = 0;
+        for (int num : nums) {
+            s ^= num;
+        }
+        return s;
+    }
+}
