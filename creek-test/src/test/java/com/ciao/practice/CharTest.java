563dd57625e546d52900e91ce0f1d73aa37e4bb7
caochengyin <caochengyin@jd.com>
2022-02-14 01:39:18 +0800

    LeetCode commit 2022/2/14

---
+++ creek-test/src/test/java/com/ciao/practice/CharTest.java
@@ -0,0 +1,19 @@
+package com.ciao.practice;
+
+import org.junit.Test;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/2/12
+ * @description char测试类
+ */
+public class CharTest {
+
+    @Test
+    public void 测试char加减法() {
+        System.out.println('2');
+        System.out.println('2' * 10);
+        System.out.println(('2' - '0') * 10);
+    }
+}
