5972a224fa3eb5cd7abc3175e65f82b37b041af7
caochengyin <caochengyin@jd.com>
2022-02-15 18:58:40 +0800

    LeetCode commit 2022/2/15

--- creek-test/src/test/java/com/ciao/leetcode/IntegerBreak.java
+++ creek-test/src/test/java/com/ciao/leetcode/IntegerBreak.java
@@ -1,7 +1,25 @@
-package com.ciao.leetcode;/**
+package com.ciao.leetcode;
+
+/**
  * @author caochengyin
  * @version v 1.0.0
  * @date 2022/2/15
- * @description 当前类の简要描述
- */public class IntegerBreak {
+ * @description https://leetcode-cn.com/problems/integer-break/
+ */
+public class IntegerBreak {
+    public static void main(String[] args) {
+        System.out.println(new IntegerBreak().integerBreak(10));
+    }
+
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int curMax = 0;
+            for (int j = 1; j < i; j++) {
+                curMax = Math.max(curMax, Math.max(j * (i - j), j * dp[i - j]));
+            }
+            dp[i] = curMax;
+        }
+        return dp[n];
+    }
 }
