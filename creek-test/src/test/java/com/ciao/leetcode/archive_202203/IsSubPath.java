bc0126cf0d47d0f3a49a84e33a87335961c06258
caochengyin <caochengyin@jd.com>
2022-03-06 23:12:49 +0800

    LeetCode commit 2022/3/6

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202203/IsSubPath.java
@@ -0,0 +1,73 @@
+package com.ciao.leetcode.archive_202203;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/3/6
+ * @description https://leetcode-cn.com/problems/linked-list-in-binary-tree/
+ */
+public class IsSubPath {
+    public static void main(String[] args) {
+        
+    }
+
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if (root == null) {
+            return false;
+        }
+        return dfs(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+
+    private boolean dfs(ListNode head, TreeNode node) {
+        if (head == null) {
+            return true;
+        }
+
+        if (node == null) {
+            return false;
+        }
+
+        if (head.val != node.val) {
+            return false;
+        }
+
+        return dfs(head.next, node.left) || dfs(head.next, node.right);
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
