b1b555743354c4f3f8572bfebb2e3560991a3db8
ciao-cao-offical <843078365@qq.com>
2022-03-17 19:51:47 +0800

    LeetCode commit 2022/3/17

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202203/GetIntersectionNode.java
@@ -0,0 +1,27 @@
+package com.ciao.leetcode.archive_202203;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/3/17
+ * @description https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+ */
+public class GetIntersectionNode {
+    public static void main(String[] args) {
+        
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        return null;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202203/LongestWord.java
@@ -0,0 +1,48 @@
+package com.ciao.leetcode.archive_202203;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/3/17
+ * @description https://leetcode-cn.com/problems/longest-word-in-dictionary/
+ * ☆☆ 学习一下【字典树】
+ */
+public class LongestWord {
+    public static void main(String[] args) {
+        String[] words = {"w", "wo", "wor", "worl", "world"};
+        System.out.println(new LongestWord().longestWord(words));
+    }
+
+    public String longestWord(String[] words) {
+        Set<String> set = new HashSet<>(Arrays.asList(words));
+
+        String ans = "";
+        for (String s : set) {
+            int sl = s.length();
+            int al = ans.length();
+            if (sl < al) {
+                continue;
+            }
+            if (sl == al && s.compareTo(ans) > 0) {
+                continue;
+            }
+
+            boolean pass = true;
+            for (int i = 0; i < sl; i++) {
+                if (!set.contains(s.substring(0, i + 1))) {
+                    pass = false;
+                    break;
+                }
+            }
+
+            if (pass) {
+                ans = s;
+            }
+        }
+        return ans;
+    }
+}
