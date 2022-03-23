3f1f495915d5c11a50a5987e62d91e2bfeb0de25
ciao <843078365@qq.com>
2022-03-23 23:54:28 +0800

    LeetCode commit 2022/3/23

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202203/FindKthNumber.java
@@ -0,0 +1,44 @@
+package com.ciao.leetcode.archive_202203;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/3/23 0023
+ * @description https://leetcode-cn.com/problems/k-th-smallest-in-lexicographical-order/
+ * ☆☆☆☆☆
+ */
+public class FindKthNumber {
+    public static void main(String[] args) {
+        System.out.println(new FindKthNumber().findKthNumber(13, 2));
+    }
+
+    public int findKthNumber(int n, int k) {
+        int cur = 1;
+        k--;
+        while (k > 0) {
+            int steps = getSteps(cur, n);
+            if (steps <= k) {
+                k -= steps;
+                cur++;
+
+            } else {
+                cur *= 10;
+                k--;
+            }
+        }
+
+        return cur;
+    }
+
+    private int getSteps(int cur, long n) {
+        int steps = 0;
+        long first = cur;
+        long last = cur;
+        while (first <= n) {
+            steps += Math.min(last, n) - first + 1;
+            first *= 10;
+            last = last * 10 + 9;
+        }
+        return steps;
+    }
+}
---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202203/MaxSubArray.java
@@ -0,0 +1,26 @@
+package com.ciao.leetcode.archive_202203;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/3/23 0023
+ * @description https://leetcode-cn.com/problems/maximum-subarray/
+ * ☆ 尝试一下【分治算法】
+ */
+public class MaxSubArray {
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(new MaxSubArray().maxSubArray(nums));
+    }
+
+    public int maxSubArray(int[] nums) {
+        int pre = 0;
+        int maxSubSum = nums[0];
+        for (int num : nums) {
+            pre = Math.max(pre + num, num);
+            maxSubSum = Math.max(maxSubSum, pre);
+        }
+
+        return maxSubSum;
+    }
+}
