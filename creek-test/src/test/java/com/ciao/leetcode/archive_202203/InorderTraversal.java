ad6450aa655721221d31883da0678403b93447bb
ciao-cao-offical <843078365@qq.com>
2022-03-29 20:00:39 +0800

    LeetCode commit 2022/3/29

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202203/InorderTraversal.java
@@ -0,0 +1,57 @@
+package com.ciao.leetcode.archive_202203;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/3/28
+ * @description https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+ */
+public class InorderTraversal {
+    public static void main(String[] args) {
+
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+
+        Deque<TreeNode> stack = new LinkedList<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+            ans.add(root.val);
+            root = root.right;
+        }
+        return ans;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
