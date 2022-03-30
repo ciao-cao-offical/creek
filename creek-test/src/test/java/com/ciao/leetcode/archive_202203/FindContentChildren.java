b6c47b6ed12b71d46912abbfa9801cc3c9713599
ciao <843078365@qq.com>
2022-03-31 00:50:09 +0800

    LeetCode commit 2022/3/31

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202203/FindContentChildren.java
@@ -0,0 +1,39 @@
+package com.ciao.leetcode.archive_202203;
+
+import java.util.Arrays;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/3/30 0030
+ * @description https://leetcode-cn.com/problems/assign-cookies/
+ */
+public class FindContentChildren {
+    public static void main(String[] args) {
+        int[] g = {10, 9, 8, 7};
+        int[] s = {5, 6, 7, 8};
+        System.out.println(new FindContentChildren().findContentChildren(g, s));
+    }
+
+    public int findContentChildren(int[] g, int[] s) {
+        if (s == null || s.length == 0) {
+            return 0;
+        }
+        int gl = g.length;
+        int sl = s.length;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count = 0;
+        for (int i = 0, j = 0; i < gl && j < sl; i++, j++) {
+            while (j < sl && g[i] > s[j]) {
+                j++;
+            }
+
+            if (j < sl) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
