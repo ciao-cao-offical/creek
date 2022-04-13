8642951c9c81d021c9161702800784ec92713800
ciao-cao-offical <843078365@qq.com>
2022-04-13 19:22:08 +0800

    LeetCode commit 2022/04/13

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202204/RandomizedSet.java
@@ -0,0 +1,37 @@
+package com.ciao.leetcode.archive_202204;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/4/13
+ * @description https://leetcode-cn.com/problems/insert-delete-getrandom-o1/
+ */
+public class RandomizedSet {
+    private Map<Integer, Integer> map;
+    private List<Integer> list;
+    private Random random = new Random();
+
+    public RandomizedSet() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean insert(int val) {
+        return false;
+    }
+
+    public boolean remove(int val) {
+        return false;
+    }
+
+    public int getRandom() {
+        return 0;
+    }
+}
