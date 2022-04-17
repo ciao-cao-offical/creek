a4ed8b7fa5e0896669ae61cd0c01119cbf2cc31f
ciao <843078365@qq.com>
2022-04-17 23:33:44 +0800

    LeetCode commit 2022/04/17

--- creek-test/src/test/java/com/ciao/leetcode/archive_202204/Fib.java
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202204/Fib.java
@@ -5,7 +5,7 @@ package com.ciao.leetcode.archive_202204;
  * @version v 1.0.0
  * @date 2022/4/9 0009
  * @description https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof/
- * 试试【矩阵快速幂】
+ * 试试【矩阵快速幂】和【通项公式】
  */
 public class Fib {
     public static void main(String[] args) {
