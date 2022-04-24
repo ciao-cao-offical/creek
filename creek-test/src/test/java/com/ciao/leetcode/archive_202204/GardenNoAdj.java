44163a29c89897c2143f258c8f2a1c938711c6b3
ciao-cao-offical <843078365@qq.com>
2022-04-24 19:46:52 +0800

    LeetCode commit 2022/04/24

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202204/GardenNoAdj.java
@@ -0,0 +1,21 @@
+package com.ciao.leetcode.archive_202204;
+
+import java.util.Arrays;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/4/24
+ * @description https://leetcode-cn.com/problems/flower-planting-with-no-adjacent/
+ */
+public class GardenNoAdj {
+    public static void main(String[] args) {
+        int n = 3;
+        int[][] paths = {{1, 2}, {2, 3}, {3, 1}};
+        System.out.println(Arrays.toString(new GardenNoAdj().gardenNoAdj(n, paths)));
+    }
+
+    public int[] gardenNoAdj(int n, int[][] paths) {
+        return null;
+    }
+}
