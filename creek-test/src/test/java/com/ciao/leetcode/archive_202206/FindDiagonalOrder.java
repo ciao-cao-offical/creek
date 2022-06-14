5ab65fcfcbf69a44241c6449635959201abffae5
ciao <843078365@qq.com>
2022-06-14 14:33:55 +0800

    LeetCode commit 2022/06/14

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202206/FindDiagonalOrder.java
@@ -0,0 +1,42 @@
+package com.ciao.leetcode.archive_202206;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/6/14 0014
+ * @description https://leetcode.cn/problems/diagonal-traverse/
+ */
+public class FindDiagonalOrder {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] findDiagonalOrder(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] res = new int[m * n];
+        int pos = 0;
+        for (int i = 0; i < m + n - 1; i++) {
+            if (i % 2 == 1) {
+                int x = i < n ? 0 : i - n + 1;
+                int y = i < n ? i : n - 1;
+                while (x < m && y >= 0) {
+                    res[pos] = mat[x][y];
+                    pos++;
+                    x++;
+                    y--;
+                }
+            } else {
+                int x = i < m ? i : m - 1;
+                int y = i < m ? 0 : i - m + 1;
+                while (x >= 0 && y < n) {
+                    res[pos] = mat[x][y];
+                    pos++;
+                    x--;
+                    y++;
+                }
+            }
+        }
+        return res;
+    }
+}
