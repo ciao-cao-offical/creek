9777168e0e9b210d5b7a9a3d45a6e83d94ca5a1e
ciao <843078365@qq.com>
2022-08-16 22:39:47 +0800

    LeetCode commit 2022/08/16

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202208/OrderedStream.java
@@ -0,0 +1,33 @@
+package com.ciao.leetcode.archive_202208;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/8/16 0016
+ * @description https://leetcode.cn/problems/design-an-ordered-stream/
+ */
+public class OrderedStream {
+    public static void main(String[] args) {
+
+    }
+
+    String[] stream;
+    int ptr;
+
+    public OrderedStream(int n) {
+        stream = new String[n + 1];
+        ptr = 1;
+    }
+
+    public List<String> insert(int idKey, String value) {
+        stream[idKey] = value;
+        List<String> list = new ArrayList<>();
+        while (ptr < stream.length && stream[ptr] != null) {
+            list.add(stream[ptr++]);
+        }
+        return list;
+    }
+}
