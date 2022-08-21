8b5f1633789a7681be45eb8928368e976311af9f
ciao <843078365@qq.com>
2022-08-21 21:53:09 +0800

    LeetCode commit 2022/08/21

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202208/IsPrefixOfWord.java
@@ -0,0 +1,24 @@
+package com.ciao.leetcode.archive_202208;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/8/21 0021
+ * @description https://leetcode.cn/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/
+ */
+public class IsPrefixOfWord {
+    public static void main(String[] args) {
+        String sentence = "i love eating burger", searchWord = "burg";
+        System.out.println(new IsPrefixOfWord().isPrefixOfWord(sentence, searchWord));
+    }
+
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] words = sentence.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].startsWith(searchWord)) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+}
