184ca1fa7029b655f0c86b1b8cc40c4f27556cf6
ciao <843078365@qq.com>
2022-08-27 22:22:23 +0800

    LeetCode commit 2022/08/27

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202208/WidthOfBinaryTree.java
@@ -0,0 +1,61 @@
+package com.ciao.leetcode.archive_202208;
+
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/8/27 0027
+ * @description https://leetcode.cn/problems/maximum-width-of-binary-tree/
+ * ☆☆☆ 试试【深度优先搜索】
+ */
+public class WidthOfBinaryTree {
+    public static void main(String[] args) {
+
+    }
+
+    //广度优先搜索
+    public int widthOfBinaryTree(TreeNode root) {
+        int res = 1;
+        List<Pair<TreeNode, Integer>> arr = new ArrayList<>();
+        arr.add(new Pair<>(root, 1));
+        while (!arr.isEmpty()) {
+            List<Pair<TreeNode, Integer>> tmp = new ArrayList<>();
+            for (Pair<TreeNode, Integer> pair : arr) {
+                TreeNode node = pair.getKey();
+                int index = pair.getValue();
+                if (node.left != null) {
+                    tmp.add(new Pair<>(node.left, index * 2));
+                }
+                if (node.right != null) {
+                    tmp.add(new Pair<>(node.right, index * 2 + 1));
+                }
+            }
+            res = Math.max(res, arr.get(arr.size() - 1).getValue() - arr.get(0).getValue() + 1);
+            arr = tmp;
+        }
+        return res;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
