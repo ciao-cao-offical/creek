0a1ac42fcb8b7a13cb35f35c1aa5839f4eea718e
ciao-cao-offical <843078365@qq.com>
2022-09-09 17:00:52 +0800

    LeetCode commit 2022/09/09

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202209/MinOperations.java
@@ -0,0 +1,35 @@
+package com.ciao.leetcode.archive_202209;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/9/9
+ * @description https://leetcode.cn/problems/crawler-log-folder/
+ */
+public class MinOperations {
+    public static void main(String[] args) {
+        String[] logs = {"d1/", "d2/", "../", "d21/", "./"};
+        System.out.println(new MinOperations().minOperations(logs));
+    }
+
+    public int minOperations(String[] logs) {
+        int step = 0;
+        for (String log : logs) {
+            switch (log) {
+                case "./":
+                    break;
+
+                case "../":
+                    if (step > 0) {
+                        step--;
+                    }
+                    break;
+
+                default:
+                    step++;
+                    break;
+            }
+        }
+        return step;
+    }
+}
