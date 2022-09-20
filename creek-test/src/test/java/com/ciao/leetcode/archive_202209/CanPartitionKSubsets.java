64a649d7082ba2ac52a1a3e1d90ad1848a85a79f
ciao-cao-offical <843078365@qq.com>
2022-09-20 18:42:05 +0800

    LeetCode commit 2022/09/20

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202209/CanPartitionKSubsets.java
@@ -0,0 +1,61 @@
+package com.ciao.leetcode.archive_202209;
+
+import java.util.Arrays;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/9/20
+ * @description https://leetcode.cn/problems/partition-to-k-equal-sum-subsets/
+ * ⭐⭐⭐⭐⭐ cvd
+ */
+public class CanPartitionKSubsets {
+    public static void main(String[] args) {
+        int[] nums = {4, 3, 2, 3, 5, 2, 1};
+        int k = 4;
+        System.out.println(new CanPartitionKSubsets().canPartitionKSubsets(nums, k));
+    }
+
+    //状态压缩 + 记忆化搜索
+    int[] nums;
+    int per, n;
+    boolean[] dp;
+
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        this.nums = nums;
+        int all = Arrays.stream(nums).sum();
+        if (all % k != 0) {
+            return false;
+        }
+        per = all / k;
+        Arrays.sort(nums);
+        n = nums.length;
+        if (nums[n - 1] > per) {
+            return false;
+        }
+        dp = new boolean[1 << n];
+        Arrays.fill(dp, true);
+        return dfs((1 << n) - 1, 0);
+    }
+
+    public boolean dfs(int s, int p) {
+        if (s == 0) {
+            return true;
+        }
+        if (!dp[s]) {
+            return dp[s];
+        }
+        dp[s] = false;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] + p > per) {
+                break;
+            }
+            if (((s >> i) & 1) != 0) {
+                if (dfs(s ^ (1 << i), (p + nums[i]) % per)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
