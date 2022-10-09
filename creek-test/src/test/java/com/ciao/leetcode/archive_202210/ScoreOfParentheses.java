c8793bc36e18ede6a1f7908bc03c7122a851b48e
ciao-cao-offical <843078365@qq.com>
2022-10-09 20:07:25 +0800

    LeetCode commit 2022/10/09

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202210/ScoreOfParentheses.java
@@ -0,0 +1,18 @@
+package com.ciao.leetcode.archive_202210;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/10/9
+ * @description https://leetcode.cn/problems/score-of-parentheses/
+ */
+public class ScoreOfParentheses {
+    public static void main(String[] args) {
+        String s = "(())";
+        System.out.println(new ScoreOfParentheses().scoreOfParentheses(s));
+    }
+
+    public int scoreOfParentheses(String s) {
+        return 0;
+    }
+}
