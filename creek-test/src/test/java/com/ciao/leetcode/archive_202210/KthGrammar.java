1c2bf4383c6b6f4a1071dc3f9f07b1819997a3c6
ciao <843078365@qq.com>
2022-10-20 23:38:34 +0800

    LeetCode commit 2022/10/20

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202210/KthGrammar.java
@@ -0,0 +1,26 @@
+package com.ciao.leetcode.archive_202210;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2022/10/20 0020
+ * @description https://leetcode.cn/problems/k-th-symbol-in-grammar/
+ */
+public class KthGrammar {
+    public static void main(String[] args) {
+        int n = 2, k = 2;
+        System.out.println(new KthGrammar().kthGrammar(n, k));
+    }
+
+    //【找规律 + 位运算】from：https://leetcode.cn/problems/k-th-symbol-in-grammar/solution/-by-muse-77-zw7u/
+    public int kthGrammar(int n, int k) {
+        if (k == 1) {
+            return 0;
+        }
+
+        if (k > (1 << n - 2)) {
+            return 1 ^ kthGrammar(n - 1, k - (1 << n - 2));
+        }
+        return kthGrammar(n - 1, k);
+    }
+}
