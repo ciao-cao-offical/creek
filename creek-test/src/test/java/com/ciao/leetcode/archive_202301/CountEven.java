cf1316800d322b870b19a3d9eae4f0f13eeb491d
ciao-cao-offical <843078365@qq.com>
2023-01-06 19:03:55 +0800

    LeetCode commit 2023/01/06

--- creek-test/src/test/java/com/ciao/leetcode/archive_202301/CountEven.java
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202301/CountEven.java
@@ -1,7 +1,29 @@
-package com.ciao.leetcode.archive_202301;/**
+package com.ciao.leetcode.archive_202301;
+
+/**
  * @author caochengyin
  * @version v 1.0.0
  * @date 2023/1/6
- * @description 简要描述当前类の功能
- */public class CountEven {
+ * @description https://leetcode.cn/problems/count-integers-with-even-digit-sum/
+ */
+public class CountEven {
+    public static void main(String[] args) {
+        int num = 910;
+        System.out.println(new CountEven().countEven(num));
+    }
+
+    public int countEven(int num) {
+        int res = 0;
+        for (int i = 2; i <= num; i++) {
+            int x = i, sum = 0;
+            while (x != 0) {
+                sum += x % 10;
+                x /= 10;
+            }
+            if (sum % 2 == 0) {
+                res++;
+            }
+        }
+        return res;
+    }
 }
