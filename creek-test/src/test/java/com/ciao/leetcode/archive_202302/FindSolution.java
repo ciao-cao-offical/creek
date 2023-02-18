f2cd5bd8532840137c12b477cdc89ca668000ead
ciao <843078365@qq.com>
2023-02-18 15:42:14 +0800

    LeetCode commit 2023/02/18

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202302/FindSolution.java
@@ -0,0 +1,50 @@
+package com.ciao.leetcode.archive_202302;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2023/2/18 0018 15:05:20
+ * @description 简要描述当前类の作用
+ */
+public class FindSolution {
+    public static void main(String[] args) {
+
+    }
+
+    //【二分法】
+    public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 1; i <= 1000; i++) {
+            int left = 1;
+            int right = 1000;
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                int f = customfunction.f(i, mid);
+                if (f == z) {
+                    List<Integer> part = new ArrayList<>();
+                    part.add(i);
+                    part.add(mid);
+                    res.add(part);
+                    break;
+                }
+                if (z < f) {
+                    right = mid - 1;
+
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return res;
+    }
+
+    interface CustomFunction {
+        // Returns f(x, y) for any given positive integers x and y.
+        // Note that f(x, y) is increasing with respect to both x and y.
+        // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+        public int f(int x, int y);
+    }
+}
