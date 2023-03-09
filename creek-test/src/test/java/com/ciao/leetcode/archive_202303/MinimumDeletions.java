2ed801a41fa7f35327e522a877e749c5d0bc04ad
ciao-cao-offical <843078365@qq.com>
2023-03-09 12:37:04 +0800

    undone push

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202303/MinimumDeletions.java
@@ -0,0 +1,17 @@
+package com.ciao.leetcode.archive_202303;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2023/3/6
+ * @description https://leetcode.cn/problems/minimum-deletions-to-make-string-balanced/
+ */
+public class MinimumDeletions {
+    public static void main(String[] args) {
+
+    }
+
+    public int minimumDeletions(String s) {
+        return 0;
+    }
+}
