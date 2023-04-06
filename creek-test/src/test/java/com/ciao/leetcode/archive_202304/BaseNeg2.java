cf5b83fb04ba6a3bb0ebc63eafb870a3ff1b6cf1
caochengyin <ciao-cao-offical>
2023-04-06 19:56:42 +0800

    LeetCode commit 2023/04/06

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202304/BaseNeg2.java
@@ -0,0 +1,27 @@
+package com.ciao.leetcode.archive_202304;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2023/4/6 19:53
+ * @description https://leetcode.cn/problems/convert-to-base-2/solutions/
+ */
+public class BaseNeg2 {
+    public static void main(String[] args) {
+
+    }
+
+    public String baseNeg2(int n) {
+        if (n == 0 || n == 1) {
+            return String.valueOf(n);
+        }
+        StringBuilder res = new StringBuilder();
+        while (n != 0) {
+            int remainder = n & 1;
+            res.append(remainder);
+            n -= remainder;
+            n /= -2;
+        }
+        return res.reverse().toString();
+    }
+}
