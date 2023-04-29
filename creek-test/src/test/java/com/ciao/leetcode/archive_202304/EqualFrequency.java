46511c26f3b30ed885daaeee9bf2534d37e4ebb6
ciao <843078365@qq.com>
2023-04-29 23:35:32 +0800

    LeetCode commit 2023/04/29

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202304/EqualFrequency.java
@@ -0,0 +1,41 @@
+package com.ciao.leetcode.archive_202304;
+
+import java.util.HashSet;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2023/4/29 0029 23:32:14
+ * @description https://leetcode.cn/problems/remove-letter-to-equalize-frequency/
+ * cvd，搬家收拾屋子，累毙...
+ */
+public class EqualFrequency {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean equalFrequency(String word) {
+        int[] charCount = new int[26];
+        int n = word.length();
+        for (int i = 0; i < n; i++) {
+            charCount[word.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (charCount[i] == 0) {
+                continue;
+            }
+            charCount[i]--;
+            HashSet<Integer> frequency = new HashSet<Integer>();
+            for (int f : charCount) {
+                if (f > 0) {
+                    frequency.add(f);
+                }
+            }
+            if (frequency.size() == 1) {
+                return true;
+            }
+            charCount[i]++;
+        }
+        return false;
+    }
+}
