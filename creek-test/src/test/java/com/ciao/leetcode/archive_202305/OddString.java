67fea0a1e282bbc25fb61debb7d84a6cfc5c701b
caochengyin <ciao-cao-offical>
2023-05-25 23:07:33 +0800

    LeetCode commit 2023/05/25

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202305/OddString.java
@@ -0,0 +1,37 @@
+package com.ciao.leetcode.archive_202305;
+
+import java.util.Arrays;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2023/5/25 21:33
+ * @description https://leetcode.cn/problems/odd-string-difference/
+ */
+public class OddString {
+    public static void main(String[] args) {
+
+    }
+
+    public String oddString(String[] words) {
+        int[] diff0 = get(words[0]);
+        int[] diff1 = get(words[1]);
+        if (Arrays.equals(diff0, diff1)) {
+            for (int i = 2; i < words.length; i++) {
+                if (!Arrays.equals(diff0, get(words[i]))) {
+                    return words[i];
+                }
+            }
+        }
+        return Arrays.equals(diff0, get(words[2])) ? words[1] : words[0];
+    }
+
+    public int[] get(String word) {
+        int[] diff = new int[word.length() - 1];
+        for (int i = 0; i + 1 < word.length(); i++) {
+            diff[i] = word.charAt(i + 1) - word.charAt(i);
+        }
+        return diff;
+    }
+
+}
