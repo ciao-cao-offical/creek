c677db3f344cc10ce153b28598966cbfb58aa8c0
ciao <843078365@qq.com>
2023-06-12 22:39:19 +0800

    LeetCode commit 2023/06/12

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202306/TreeAncestor.java
@@ -0,0 +1,48 @@
+package com.ciao.leetcode.archive_202306;
+
+import java.util.Arrays;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2023/6/12 0012 22:32:52
+ * @description https://leetcode.cn/problems/kth-ancestor-of-a-tree-node/solutions/2301511/shu-jie-dian-de-di-k-ge-zu-xian-by-leetc-hdxd/
+ * ★★★★★ cvd
+ */
+public class TreeAncestor {
+    public static void main(String[] args) {
+
+    }
+
+    static final int LOG = 16;
+    int[][] ancestors;
+
+    public TreeAncestor(int n, int[] parent) {
+        ancestors = new int[n][LOG];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(ancestors[i], -1);
+        }
+        for (int i = 0; i < n; i++) {
+            ancestors[i][0] = parent[i];
+        }
+        for (int j = 1; j < LOG; j++) {
+            for (int i = 0; i < n; i++) {
+                if (ancestors[i][j - 1] != -1) {
+                    ancestors[i][j] = ancestors[ancestors[i][j - 1]][j - 1];
+                }
+            }
+        }
+    }
+
+    public int getKthAncestor(int node, int k) {
+        for (int j = 0; j < LOG; j++) {
+            if (((k >> j) & 1) != 0) {
+                node = ancestors[node][j];
+                if (node == -1) {
+                    return -1;
+                }
+            }
+        }
+        return node;
+    }
+}
