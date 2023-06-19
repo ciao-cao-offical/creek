3a14b3cfcf743687829ef782faa387ae31434fa8
ciao <843078365@qq.com>
2023-06-19 23:54:10 +0800

    LeetCode commit 2023/06/19

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202306/MaxSumDivThree.java
@@ -0,0 +1,30 @@
+package com.ciao.leetcode.archive_202306;
+
+import java.util.Arrays;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2023/6/19 0019 22:10:18
+ * @description https://leetcode.cn/problems/greatest-sum-divisible-by-three/
+ */
+public class MaxSumDivThree {
+    public static void main(String[] args) {
+
+    }
+
+    public int maxSumDivThree(int[] nums) {
+        int[] f = {0, Integer.MIN_VALUE, Integer.MIN_VALUE};
+        for (int num : nums) {
+            int[] g = Arrays.copyOfRange(f, 0, 3);
+            for (int i = 0; i < 3; i++) {
+                int remainder = (i + num % 3) % 3;
+                g[remainder] = Math.max(g[remainder], f[i] + num);
+            }
+
+            f = g;
+        }
+
+        return f[0];
+    }
+}
