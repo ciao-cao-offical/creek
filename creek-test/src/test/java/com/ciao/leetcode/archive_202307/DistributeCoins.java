749b7565369c5ec57b0174629dc69a06d7820c83
ciao <843078365@qq.com>
2023-07-14 02:28:12 +0800

    LeetCode commit 2023/07/14

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202307/DistributeCoins.java
@@ -0,0 +1,55 @@
+package com.ciao.leetcode.archive_202307;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2023/7/14 0014 01:59:40
+ * @description https://leetcode.cn/problems/distribute-coins-in-binary-tree/
+ */
+public class DistributeCoins {
+    public static void main(String[] args) {
+
+    }
+
+    int move = 0;
+
+    public int distributeCoins(TreeNode root) {
+        dfs(root);
+        return move;
+    }
+
+    public int dfs(TreeNode root) {
+        int moveleft = 0;
+        int moveright = 0;
+        if (root == null) {
+            return 0;
+        }
+        if (root.left != null) {
+            moveleft = dfs(root.left);
+        }
+        if (root.right != null) {
+            moveright = dfs(root.right);
+        }
+        move += Math.abs(moveleft) + Math.abs(moveright);
+        return moveleft + moveright + root.val - 1;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
