fd7f41a4c78c72b3220597daa4153e4f8555cf74
ciao <843078365@qq.com>
2023-07-19 00:58:54 +0800

    LeetCode commit 2023/07/19

---
+++ creek-test/src/test/java/com/ciao/leetcode/archive_202307/RobotSim.java
@@ -0,0 +1,46 @@
+package com.ciao.leetcode.archive_202307;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2023/7/19 0019 00:16:40
+ * @description https://leetcode.cn/problems/walking-robot-simulation/
+ */
+public class RobotSim {
+    public static void main(String[] args) {
+
+    }
+
+    // 模拟
+    public int robotSim(int[] commands, int[][] obstacles) {
+        int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        int px = 0, py = 0, d = 1;
+        Set<Integer> set = new HashSet<Integer>();
+        for (int[] obstacle : obstacles) {
+            set.add(obstacle[0] * 60001 + obstacle[1]);
+        }
+        int res = 0;
+        for (int c : commands) {
+            if (c < 0) {
+                d += c == -1 ? 1 : -1;
+                d %= 4;
+                if (d < 0) {
+                    d += 4;
+                }
+            } else {
+                for (int i = 0; i < c; i++) {
+                    if (set.contains((px + dirs[d][0]) * 60001 + py + dirs[d][1])) {
+                        break;
+                    }
+                    px += dirs[d][0];
+                    py += dirs[d][1];
+                    res = Math.max(res, px * px + py * py);
+                }
+            }
+        }
+        return res;
+    }
+}
