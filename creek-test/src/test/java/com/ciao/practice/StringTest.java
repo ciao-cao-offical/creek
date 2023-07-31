75751778187dad1ab03b03910603f6f848113b4a
ciao <843078365@qq.com>
2023-07-31 19:56:29 +0800

    LeetCode commit 2023/07/31

--- creek-test/src/test/java/com/ciao/practice/StringTest.java
+++ creek-test/src/test/java/com/ciao/practice/StringTest.java
@@ -2,6 +2,9 @@ package com.ciao.practice;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author caochengyin
  * @version v 1.0.0
@@ -24,5 +27,22 @@ public class StringTest {
         System.out.println(test_str.startsWith("01", 1));
         System.out.println(test_str.startsWith("01", 2));
         System.out.println(test_str.startsWith("01", 3));
+    }
+
+    @Test
+    public void 测试ArrayList的add方法() {
+        Integer[] alphas = {100001, 100002, 100003, 100001};
+        int i = 0;
+        Integer s = 0;
+        List<Integer> list = new ArrayList<>();
+        while (i < alphas.length) {
+            s = alphas[i];
+            list.add(s);
+
+            i++;
+        }
+
+        System.out.println(list);
+
     }
 }
