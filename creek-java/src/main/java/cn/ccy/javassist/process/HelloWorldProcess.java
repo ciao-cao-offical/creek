e4f9cd9f4bb26ba9d65ce8bacfd6dc2f63e06def
ciao <843078365@qq.com>
2023-08-14 01:19:08 +0800

    LeetCode commit 2023/08/14

--- creek-java/src/main/java/cn/ccy/javassist/process/HelloWorldProcess.java
+++ creek-java/src/main/java/cn/ccy/javassist/process/HelloWorldProcess.java
@@ -1,7 +1,14 @@
 package cn.ccy.javassist.process;
 
 import cn.ccy.javassist.origin.HelloWorld;
-import javassist.*;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.CtNewMethod;
+import lombok.SneakyThrows;
+
+import java.io.File;
+import java.nio.file.Files;
 
 /**
  * @author caochengyin
@@ -10,10 +17,18 @@ import javassist.*;
  * @description HelloWorld打印类字节码增强
  */
 public class HelloWorldProcess {
-    public static void main(String[] args) throws NotFoundException, CannotCompileException {
+    public static void main(String[] args) {
+        // 以下方法不可以同时执行，会出现以下报错
+        // Exception in thread "main" javassist.CannotCompileException: by java.lang.ClassFormatError: loader (instance of  sun/misc/Launcher$AppClassLoader): attempted  duplicate class definition for name: "cn/ccy/javassist/origin/HelloWorld"
+//        入门();
+        入门2_方法拷贝();
+    }
+
+    @SneakyThrows
+    public static void 入门() {
         ClassPool cp = new ClassPool();
         cp.appendSystemPath();
-        
+
         CtClass ctClass = cp.get("cn.ccy.javassist.origin.HelloWorld");
         CtMethod method = ctClass.getDeclaredMethods()[0];
         /*long start = System.currentTimeMillis();
@@ -29,5 +44,29 @@ public class HelloWorldProcess {
 
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.print();
+    }
+
+    @SneakyThrows
+    public static void 入门2_方法拷贝() {
+        ClassPool cp = ClassPool.getDefault();
+        cp.appendSystemPath();
+        CtClass ctClass = cp.get("cn.ccy.javassist.origin.HelloWorld");
+        CtMethod method = ctClass.getDeclaredMethods()[0];
+        CtMethod copy = CtNewMethod.copy(method, ctClass, null);
+        copy.setName(method.getName() + "$agent");
+        ctClass.addMethod(copy);
+        method.setBody(String.format("{long start = System.currentTimeMillis();\n" +
+                "        try {\n" +
+                "            String methodName = \"%s\";\n" +
+                "            System.out.println(\"方法执行前...\");\n" +
+                "            %s$agent($$);\n" +
+                "            System.out.println(\"方法执行后...\");\n" +
+                "        } finally {\n" +
+                "            long end = System.currentTimeMillis();\n" +
+                "            System.out.println(\"方法执行完成 共计耗时：\" + (end - start));\n" +
+                "        }}", method.getName(), method.getName()));
+        ctClass.toClass();
+        Files.write(new File(System.getProperty("user.dir") + "/creek-java/target/HelloWorld.class").toPath(), ctClass.toBytecode());
+        new HelloWorld().print();
     }
 }
