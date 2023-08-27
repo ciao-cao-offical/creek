1a1729069b20f742fd3b7c34433c4a810374ac9c
ciao <843078365@qq.com>
2023-08-28 00:30:55 +0800

    LeetCode commit 2023/08/28

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202308/Insert.java
@@ -0,0 +1,49 @@
+package cn.ccy.leetcode.archive_202308;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2023/8/28 0028 00:24:34
+ * @description https://leetcode.cn/problems/insert-interval/
+ */
+public class Insert {
+    public static void main(String[] args) {
+
+    }
+
+    // cvd，快乐周末，快乐dota2！吼吼吼
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int left = newInterval[0];
+        int right = newInterval[1];
+        boolean placed = false;
+        List<int[]> ansList = new ArrayList<int[]>();
+        for (int[] interval : intervals) {
+            if (interval[0] > right) {
+                // 在插入区间的右侧且无交集
+                if (!placed) {
+                    ansList.add(new int[]{left, right});
+                    placed = true;
+                }
+                ansList.add(interval);
+            } else if (interval[1] < left) {
+                // 在插入区间的左侧且无交集
+                ansList.add(interval);
+            } else {
+                // 与插入区间有交集，计算它们的并集
+                left = Math.min(left, interval[0]);
+                right = Math.max(right, interval[1]);
+            }
+        }
+        if (!placed) {
+            ansList.add(new int[]{left, right});
+        }
+        int[][] ans = new int[ansList.size()][2];
+        for (int i = 0; i < ansList.size(); ++i) {
+            ans[i] = ansList.get(i);
+        }
+        return ans;
+    }
+}
