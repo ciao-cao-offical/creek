91f41f4303b90603d9b93674a8baf710bf0e4f23
caochengyin <caochengyin@jd.com>
2023-10-03 22:23:05 +0800

    LeetCode commit 2023/10/03

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202310/MaxProfit.java
@@ -0,0 +1,7 @@
+package cn.ccy.leetcode.archive_202310;/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2023/10/1 23:23
+ * @description 简要描述当前类功能
+ */public class MaxProfit {
+}
---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202310/MaxProfit2.java
@@ -0,0 +1,7 @@
+package cn.ccy.leetcode.archive_202310;/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2023/10/2 11:37
+ * @description 简要描述当前类功能
+ */public class MaxProfit2 {
+}
