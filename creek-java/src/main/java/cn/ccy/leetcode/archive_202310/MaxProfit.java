616360ca9f7a2134f54e94eec066bac8ef178a4c
caochengyin <caochengyin@jd.com>
2023-10-03 22:23:29 +0800

    LeetCode commit 2023/10/03

--- creek-java/src/main/java/cn/ccy/leetcode/archive_202310/MaxProfit.java
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202310/MaxProfit.java
@@ -1,7 +1,26 @@
-package cn.ccy.leetcode.archive_202310;/**
+package cn.ccy.leetcode.archive_202310;
+
+/**
  * @author caochengyin
  * @version v 1.0.0
  * @date 2023/10/1 23:23
- * @description 简要描述当前类功能
- */public class MaxProfit {
+ * @description https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/?envType=daily-question&envId=2023-10-01
+ */
+public class MaxProfit {
+    public static void main(String[] args) {
+
+    }
+
+    public int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice) {
+                minprice = prices[i];
+            } else if (prices[i] - minprice > maxprofit) {
+                maxprofit = prices[i] - minprice;
+            }
+        }
+        return maxprofit;
+    }
 }
