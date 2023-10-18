d0e3187afcbb9010ded7b2557291848f4d43609a
caochengyin <caochengyin@jd.com>
2023-10-18 18:58:25 +0800

    Join in 2023 JD 1024 celebration

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202310/Joy.java
@@ -0,0 +1,14 @@
+package cn.ccy.leetcode.archive_202310;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2023/10/18 18:50
+ * @description JOY 1024
+ */
+public class Joy {
+    public static void main(String[] args) {
+        // 打印输出"Hello 1023!"
+        System.out.println("Hello 1023!");
+    }
+}
