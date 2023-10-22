5d1d747475a6f85c0ee0de5f628ee47b7be723ed
ciao <843078365@qq.com>
2023-10-23 01:25:05 +0800

    LeetCode commit 2023/10/23

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202310/CountSeniors.java
@@ -0,0 +1,24 @@
+package cn.ccy.leetcode.archive_202310;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2023/10/23 0023 01:22:11
+ * @description https://leetcode.cn/problems/number-of-senior-citizens/?envType=daily-question&envId=2023-10-23
+ */
+public class CountSeniors {
+    public static void main(String[] args) {
+
+    }
+
+    public int countSeniors(String[] details) {
+        int count = 0;
+        for (String detail : details) {
+            int age = Integer.parseInt(detail.substring(11, 13));
+            if (age > 60) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
