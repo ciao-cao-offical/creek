1823331bd9d213d365d772e60edfe7735e1f138b
ciao <843078365@qq.com>
2023-10-25 01:17:17 +0800

    LeetCode commit 2023/10/25

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202310/PunishmentNumber.java
@@ -0,0 +1,42 @@
+package cn.ccy.leetcode.archive_202310;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2023/10/25 0025 01:15:27
+ * @description https://leetcode.cn/problems/find-the-punishment-number-of-an-integer/?envType=daily-question&envId=2023-10-25
+ * ☆☆☆ cvd，感冒ing 🤧
+ */
+public class PunishmentNumber {
+    public static void main(String[] args) {
+
+    }
+
+    public int punishmentNumber(int n) {
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            String s = Integer.toString(i * i);
+            if (dfs(s, 0, 0, i)) {
+                res += i * i;
+            }
+        }
+        return res;
+    }
+
+    public boolean dfs(String s, int pos, int tot, int target) {
+        if (pos == s.length()) {
+            return tot == target;
+        }
+        int sum = 0;
+        for (int i = pos; i < s.length(); i++) {
+            sum = sum * 10 + s.charAt(i) - '0';
+            if (sum + tot > target) {
+                break;
+            }
+            if (dfs(s, i + 1, sum + tot, target)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
