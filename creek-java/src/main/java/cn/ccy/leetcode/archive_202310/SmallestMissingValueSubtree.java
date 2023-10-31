a86219944c3b56b0b91ec4a2235e3287480197a3
caochengyin <caochengyin@jd.com>
2023-10-31 21:53:44 +0800

    LeetCode commit 2023/10/31

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202310/SmallestMissingValueSubtree.java
@@ -0,0 +1,53 @@
+package cn.ccy.leetcode.archive_202310;
+
+import java.util.*;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2023/10/31 21:51
+ * @description https://leetcode.cn/problems/smallest-missing-genetic-value-in-each-subtree/?envType=daily-question&envId=2023-10-31
+ * ⭐️⭐️⭐️⭐️⭐️ cvd，今天双十一开门红啦！
+ */
+public class SmallestMissingValueSubtree {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] smallestMissingValueSubtree(int[] parents, int[] nums) {
+        int n = parents.length;
+        List<Integer>[] children = new List[n];
+        for (int i = 0; i < n; i++) {
+            children[i] = new ArrayList<Integer>();
+        }
+        for (int i = 1; i < n; i++) {
+            children[parents[i]].add(i);
+        }
+
+        int[] res = new int[n];
+        Arrays.fill(res, 1);
+        Set<Integer>[] geneSet = new Set[n];
+        for (int i = 0; i < n; i++) {
+            geneSet[i] = new HashSet<Integer>();
+        }
+        dfs(0, res, nums, children, geneSet);
+        return res;
+    }
+
+    public int dfs(int node, int[] res, int[] nums, List<Integer>[] children, Set<Integer>[] geneSet) {
+        geneSet[node].add(nums[node]);
+        for (int child : children[node]) {
+            res[node] = Math.max(res[node], dfs(child, res, nums, children, geneSet));
+            if (geneSet[node].size() < geneSet[child].size()) {
+                Set<Integer> temp = geneSet[node];
+                geneSet[node] = geneSet[child];
+                geneSet[child] = temp;
+            }
+            geneSet[node].addAll(geneSet[child]);
+        }
+        while (geneSet[node].contains(res[node])) {
+            res[node]++;
+        }
+        return res[node];
+    }
+}
