499525f277340e587f1aa9e5b9e68dc2d854218b
ciao <843078365@qq.com>
2023-11-26 21:28:58 +0800

    LeetCode commit 2023/11/26

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202311/UniqueLetterString.java
@@ -0,0 +1,39 @@
+package cn.ccy.leetcode.archive_202311;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2023/11/26 0026 21:27:04
+ * @description https://leetcode.cn/problems/count-unique-characters-of-all-substrings-of-a-given-string/?envType=daily-question&envId=2023-11-26
+ */
+public class UniqueLetterString {
+    public static void main(String[] args) {
+
+    }
+
+    public int uniqueLetterString(String s) {
+        Map<Character, List<Integer>> index = new HashMap<Character, List<Integer>>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!index.containsKey(c)) {
+                index.put(c, new ArrayList<Integer>());
+                index.get(c).add(-1);
+            }
+            index.get(c).add(i);
+        }
+        int res = 0;
+        for (Map.Entry<Character, List<Integer>> entry : index.entrySet()) {
+            List<Integer> arr = entry.getValue();
+            arr.add(s.length());
+            for (int i = 1; i < arr.size() - 1; i++) {
+                res += (arr.get(i) - arr.get(i - 1)) * (arr.get(i + 1) - arr.get(i));
+            }
+        }
+        return res;
+    }
+}
