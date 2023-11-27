faa6879c8d3a31cbe9427744042baec6b9273f82
ciao <843078365@qq.com>
2023-11-28 00:18:26 +0800

    LeetCode commit 2023/11/28

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202311/FrontMiddleBackQueue.java
@@ -0,0 +1,84 @@
+package cn.ccy.leetcode.archive_202311;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2023/11/28 0028 00:15:41
+ * @description https://leetcode.cn/problems/design-front-middle-back-queue/description/?envType=daily-question&envId=2023-11-28
+ * ☆☆☆ 2023b2t
+ */
+public class FrontMiddleBackQueue {
+    public static void main(String[] args) {
+
+    }
+
+    Deque<Integer> left;
+    Deque<Integer> right;
+
+    public FrontMiddleBackQueue() {
+        left = new ArrayDeque<Integer>();
+        right = new ArrayDeque<Integer>();
+    }
+
+    public void pushFront(int val) {
+        left.offerFirst(val);
+        if (left.size() == right.size() + 2) {
+            right.offerFirst(left.pollLast());
+        }
+    }
+
+    public void pushMiddle(int val) {
+        if (left.size() == right.size() + 1) {
+            right.offerFirst(left.pollLast());
+        }
+        left.offerLast(val);
+    }
+
+    public void pushBack(int val) {
+        right.offerLast(val);
+        if (left.size() + 1 == right.size()) {
+            left.offerLast(right.pollFirst());
+        }
+    }
+
+    public int popFront() {
+        if (left.isEmpty()) {
+            return -1;
+        }
+        int val = left.pollFirst();
+        if (left.size() + 1 == right.size()) {
+            left.offerLast(right.pollFirst());
+        }
+        return val;
+    }
+
+    public int popMiddle() {
+        if (left.isEmpty()) {
+            return -1;
+        }
+        int val = left.pollLast();
+        if (left.size() + 1 == right.size()) {
+            left.offerLast(right.pollFirst());
+        }
+        return val;
+    }
+
+    public int popBack() {
+        if (left.isEmpty()) {
+            return -1;
+        }
+        int val = 0;
+        if (right.isEmpty()) {
+            val = left.pollLast();
+        } else {
+            val = right.pollLast();
+            if (left.size() == right.size() + 2) {
+                right.offerFirst(left.pollLast());
+            }
+        }
+        return val;
+    }
+}
