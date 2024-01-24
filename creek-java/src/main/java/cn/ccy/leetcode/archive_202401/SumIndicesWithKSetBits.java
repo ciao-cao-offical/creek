e2e8d3a0846059efa40b02a987a72d38983cc96a
ciao <843078365@qq.com>
2024-01-25 01:14:09 +0800

    LeetCode commit 2024/01/25

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202401/SumIndicesWithKSetBits.java
@@ -0,0 +1,34 @@
+package cn.ccy.leetcode.archive_202401;
+
+import java.util.List;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2024/1/25 0025 00:54:39
+ * @description https://leetcode.cn/problems/sum-of-values-at-indices-with-k-set-bits/?envType=daily-question&envId=2024-01-25
+ */
+public class SumIndicesWithKSetBits {
+    public static void main(String[] args) {
+
+    }
+
+    public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (bitCount(i) == k) {
+                ans += nums.get(i);
+            }
+        }
+        return ans;
+    }
+
+    public int bitCount(int x) {
+        int cnt = 0;
+        while (x != 0) {
+            cnt += (x % 2);
+            x /= 2;
+        }
+        return cnt;
+    }
+}
