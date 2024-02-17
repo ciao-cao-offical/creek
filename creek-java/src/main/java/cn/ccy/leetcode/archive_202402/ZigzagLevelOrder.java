73b1c3fed7c482872f66953d315d5d551e2be7f8
ciao <843078365@qq.com>
2024-02-17 21:31:43 +0800

    LeetCode commit 2024/02/17

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202402/ZigzagLevelOrder.java
@@ -0,0 +1,68 @@
+package cn.ccy.leetcode.archive_202402;
+
+import java.util.*;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2024/2/16 0016 22:55:49
+ * @description https://leetcode.cn/problems/binary-tree-zigzag-level-order-traversal/?envType=daily-question&envId=2024-02-16
+ */
+public class ZigzagLevelOrder {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new LinkedList<List<Integer>>();
+        if (root == null) {
+            return ans;
+        }
+
+        Queue<TreeNode> nodeQueue = new ArrayDeque<TreeNode>();
+        nodeQueue.offer(root);
+        boolean isOrderLeft = true;
+
+        while (!nodeQueue.isEmpty()) {
+            Deque<Integer> levelList = new LinkedList<Integer>();
+            int size = nodeQueue.size();
+            for (int i = 0; i < size; ++i) {
+                TreeNode curNode = nodeQueue.poll();
+                if (isOrderLeft) {
+                    levelList.offerLast(curNode.val);
+                } else {
+                    levelList.offerFirst(curNode.val);
+                }
+                if (curNode.left != null) {
+                    nodeQueue.offer(curNode.left);
+                }
+                if (curNode.right != null) {
+                    nodeQueue.offer(curNode.right);
+                }
+            }
+            ans.add(new LinkedList<Integer>(levelList));
+            isOrderLeft = !isOrderLeft;
+        }
+
+        return ans;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
