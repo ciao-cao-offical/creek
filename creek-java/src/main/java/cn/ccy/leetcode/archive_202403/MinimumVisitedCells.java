5b2066957c6489eeb047b82def509be894c288ea
ciao <843078365@qq.com>
2024-03-22 00:12:01 +0800

    LeetCode commit 2024/03/22

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202403/MinimumVisitedCells.java
@@ -0,0 +1,63 @@
+package cn.ccy.leetcode.archive_202403;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2024/3/22 0022 00:10:28
+ * @description https://leetcode.cn/problems/minimum-number-of-visited-cells-in-a-grid/?envType=daily-question&envId=2024-03-22
+ * ★★★★★ 2024cvd
+ */
+public class MinimumVisitedCells {
+    public static void main(String[] args) {
+
+    }
+
+    public int minimumVisitedCells(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[][] dist = new int[m][n];
+        for (int i = 0; i < m; ++i) {
+            Arrays.fill(dist[i], -1);
+        }
+        dist[0][0] = 1;
+        PriorityQueue<int[]>[] row = new PriorityQueue[m];
+        PriorityQueue<int[]>[] col = new PriorityQueue[n];
+        for (int i = 0; i < m; ++i) {
+            row[i] = new PriorityQueue<int[]>((a, b) -> a[0] - b[0]);
+        }
+        for (int i = 0; i < n; ++i) {
+            col[i] = new PriorityQueue<int[]>((a, b) -> a[0] - b[0]);
+        }
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                while (!row[i].isEmpty() && row[i].peek()[1] + grid[i][row[i].peek()[1]] < j) {
+                    row[i].poll();
+                }
+                if (!row[i].isEmpty()) {
+                    dist[i][j] = update(dist[i][j], dist[i][row[i].peek()[1]] + 1);
+                }
+
+                while (!col[j].isEmpty() && col[j].peek()[1] + grid[col[j].peek()[1]][j] < i) {
+                    col[j].poll();
+                }
+                if (!col[j].isEmpty()) {
+                    dist[i][j] = update(dist[i][j], dist[col[j].peek()[1]][j] + 1);
+                }
+
+                if (dist[i][j] != -1) {
+                    row[i].offer(new int[]{dist[i][j], j});
+                    col[j].offer(new int[]{dist[i][j], i});
+                }
+            }
+        }
+
+        return dist[m - 1][n - 1];
+    }
+
+    public int update(int x, int y) {
+        return x == -1 || y < x ? y : x;
+    }
+}
