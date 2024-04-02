8c150adadc513db203f5e685b1b413685a524368
ciao <843078365@qq.com>
2024-04-02 21:46:42 +0800

    LeetCode commit 2024/04/02

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202404/AllPossibleFBT.java
@@ -0,0 +1,57 @@
+package cn.ccy.leetcode.archive_202404;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2024/4/2 0002 21:39:37
+ * @description https://leetcode.cn/problems/all-possible-full-binary-trees/?envType=daily-question&envId=2024-04-02
+ */
+public class AllPossibleFBT {
+    public static void main(String[] args) {
+
+    }
+    
+    public List<TreeNode> allPossibleFBT(int n) {
+        List<TreeNode> fullBinaryTrees = new ArrayList<TreeNode>();
+        if (n % 2 == 0) {
+            return fullBinaryTrees;
+        }
+        if (n == 1) {
+            fullBinaryTrees.add(new TreeNode(0));
+            return fullBinaryTrees;
+        }
+        for (int i = 1; i < n; i += 2) {
+            List<TreeNode> leftSubtrees = allPossibleFBT(i);
+            List<TreeNode> rightSubtrees = allPossibleFBT(n - 1 - i);
+            for (TreeNode leftSubtree : leftSubtrees) {
+                for (TreeNode rightSubtree : rightSubtrees) {
+                    TreeNode root = new TreeNode(0, leftSubtree, rightSubtree);
+                    fullBinaryTrees.add(root);
+                }
+            }
+        }
+        return fullBinaryTrees;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
