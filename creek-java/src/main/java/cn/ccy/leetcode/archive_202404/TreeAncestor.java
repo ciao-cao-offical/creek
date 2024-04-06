9de284f8572c58cf6b4558bb24fdbcdfb85cea02
ciao <843078365@qq.com>
2024-04-06 12:41:42 +0800

    LeetCode commit 2024/04/06

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202404/TreeAncestor.java
@@ -0,0 +1,48 @@
+package cn.ccy.leetcode.archive_202404;
+
+import java.util.Arrays;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2024/4/6 12:39:21
+ * @description https://leetcode.cn/problems/kth-ancestor-of-a-tree-node/description/?envType=daily-question&envId=2024-04-06
+ * ☆☆☆☆☆ 2024cvd
+ */
+public class TreeAncestor {
+    public static void main(String[] args) {
+
+    }
+
+    static final int LOG = 16;
+    int[][] ancestors;
+
+    public TreeAncestor(int n, int[] parent) {
+        ancestors = new int[n][LOG];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(ancestors[i], -1);
+        }
+        for (int i = 0; i < n; i++) {
+            ancestors[i][0] = parent[i];
+        }
+        for (int j = 1; j < LOG; j++) {
+            for (int i = 0; i < n; i++) {
+                if (ancestors[i][j - 1] != -1) {
+                    ancestors[i][j] = ancestors[ancestors[i][j - 1]][j - 1];
+                }
+            }
+        }
+    }
+
+    public int getKthAncestor(int node, int k) {
+        for (int j = 0; j < LOG; j++) {
+            if (((k >> j) & 1) != 0) {
+                node = ancestors[node][j];
+                if (node == -1) {
+                    return -1;
+                }
+            }
+        }
+        return node;
+    }
+}
