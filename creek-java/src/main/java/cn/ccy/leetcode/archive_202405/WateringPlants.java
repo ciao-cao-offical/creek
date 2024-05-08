81dbb476bd2d2be87c1125f3cb00a1e3dbb25814
ciao <843078365@qq.com>
2024-05-08 23:10:46 +0800

    LeetCode commit 2024/05/08

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202405/WateringPlants.java
@@ -0,0 +1,29 @@
+package cn.ccy.leetcode.archive_202405;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2024/5/8 23:07:22
+ * @description https://leetcode.cn/problems/watering-plants/?envType=daily-question&envId=2024-05-08
+ */
+public class WateringPlants {
+    public static void main(String[] args) {
+
+    }
+
+    public int wateringPlants(int[] plants, int capacity) {
+        int n = plants.length;
+        int ans = 0;
+        int rest = capacity;
+        for (int i = 0; i < n; ++i) {
+            if (rest >= plants[i]) {
+                ++ans;
+                rest -= plants[i];
+            } else {
+                ans += i * 2 + 1;
+                rest = capacity - plants[i];
+            }
+        }
+        return ans;
+    }
+}
