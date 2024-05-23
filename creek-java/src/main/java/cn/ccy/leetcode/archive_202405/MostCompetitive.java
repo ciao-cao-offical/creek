dfb4ac8824768d61800ed3cb6bb1afc8233e529f
ciao <843078365@qq.com>
2024-05-24 00:06:32 +0800

    LeetCode commit 2024/05/24

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202405/MostCompetitive.java
@@ -0,0 +1,7 @@
+package cn.ccy.leetcode.archive_202405;/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2024/5/24 00:05:25
+ * @description 简要描述当前类の作用
+ */public class MostCompetitive {
+}
