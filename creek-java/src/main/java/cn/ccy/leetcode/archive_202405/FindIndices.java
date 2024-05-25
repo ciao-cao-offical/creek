9de4f2dd16d5d15a7c201a8a0d74cea743bc81ea
ciao <843078365@qq.com>
2024-05-25 21:38:26 +0800

    LeetCode commit 2024/05/25

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202405/FindIndices.java
@@ -0,0 +1,27 @@
+package cn.ccy.leetcode.archive_202405;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2024/5/25 21:30:19
+ * @description https://leetcode.cn/problems/find-indices-with-index-and-value-difference-i/?envType=daily-question&envId=2024-05-25
+ */
+public class FindIndices {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] findIndices(int[] nums, int indexDifference, int valueDifference) {
+        int[] res = {-1, -1};
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (Math.abs(i - j) >= indexDifference && Math.abs(nums[i] - nums[j]) >= valueDifference) {
+                    res[0] = i;
+                    res[1] = j;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
