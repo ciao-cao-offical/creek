a3dafb7089f7bc9f8c64a4f4aa7c94e436d4c616
ciao <843078365@qq.com>
2024-09-17 21:22:45 +0800

    feat(*):LeetCode commit 2024/09/17

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202409/NumBusesToDestination.java
@@ -0,0 +1,61 @@
+package cn.ccy.leetcode.archive_202409;
+
+import java.util.*;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2024/9/17 21:18:15
+ * @description https://leetcode.cn/problems/bus-routes/?envType=daily-question&envId=2024-09-17
+ * ☆☆☆☆☆ 2024cvd
+ */
+public class NumBusesToDestination {
+    public static void main(String[] args) {
+
+    }
+
+    public int numBusesToDestination(int[][] routes, int source, int target) {
+        if (source == target) {
+            return 0;
+        }
+
+        int n = routes.length;
+        boolean[][] edge = new boolean[n][n];
+        Map<Integer, List<Integer>> rec = new HashMap<Integer, List<Integer>>();
+        for (int i = 0; i < n; i++) {
+            for (int site : routes[i]) {
+                List<Integer> list = rec.getOrDefault(site, new ArrayList<Integer>());
+                for (int j : list) {
+                    edge[i][j] = edge[j][i] = true;
+                }
+                list.add(i);
+                rec.put(site, list);
+            }
+        }
+
+        int[] dis = new int[n];
+        Arrays.fill(dis, -1);
+        Queue<Integer> que = new LinkedList<Integer>();
+        for (int bus : rec.getOrDefault(source, new ArrayList<Integer>())) {
+            dis[bus] = 1;
+            que.offer(bus);
+        }
+        while (!que.isEmpty()) {
+            int x = que.poll();
+            for (int y = 0; y < n; y++) {
+                if (edge[x][y] && dis[y] == -1) {
+                    dis[y] = dis[x] + 1;
+                    que.offer(y);
+                }
+            }
+        }
+
+        int ret = Integer.MAX_VALUE;
+        for (int bus : rec.getOrDefault(target, new ArrayList<Integer>())) {
+            if (dis[bus] != -1) {
+                ret = Math.min(ret, dis[bus]);
+            }
+        }
+        return ret == Integer.MAX_VALUE ? -1 : ret;
+    }
+}
