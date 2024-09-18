e5885a5ecca46ab158ff4f6f2ec2fcbeb9a18c9d
ciao <843078365@qq.com>
2024-09-19 00:13:09 +0800

    feat(*):LeetCode commit 2024/09/19

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202409/LongestContinuousSubstring.java
@@ -0,0 +1,27 @@
+package cn.ccy.leetcode.archive_202409;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2024/9/19 00:10:59
+ * @description https://leetcode.cn/problems/length-of-the-longest-alphabetical-continuous-substring/?envType=daily-question&envId=2024-09-19
+ */
+public class LongestContinuousSubstring {
+    public static void main(String[] args) {
+
+    }
+
+    public int longestContinuousSubstring(String s) {
+        int res = 1;
+        int cur = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1) + 1) {
+                cur++;
+            } else {
+                cur = 1;
+            }
+            res = Math.max(res, cur);
+        }
+        return res;
+    }
+}
