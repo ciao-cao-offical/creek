e49999d90c38cd318b4980a758e273844ac7df0f
caochengyin <caochengyin@jd.com>
2024-10-03 19:52:55 +0800

    feat(creek-java):LeetCode commit 2024/10/03

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202410/MinCost.java
@@ -0,0 +1,43 @@
+package cn.ccy.leetcode.archive_202410;
+
+import java.util.Arrays;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2024/10/3 19:43
+ * @description https://leetcode.cn/problems/minimum-cost-to-reach-destination-in-time/?envType=daily-question&envId=2024-10-03
+ * ⭐️⭐️⭐️⭐️⭐️ 2024cvd
+ */
+public class MinCost {
+    public static void main(String[] args) {
+
+    }
+
+    public int minCost(int maxTime, int[][] edges, int[] passingFees) {
+        int n = passingFees.length;
+        int[][] f = new int[maxTime + 1][n];
+        for (int i = 0; i <= maxTime; i++) {
+            Arrays.fill(f[i], Integer.MAX_VALUE);
+        }
+        f[0][0] = passingFees[0];
+        for (int t = 1; t <= maxTime; t++) {
+            for (int[] edge : edges) {
+                int i = edge[0], j = edge[1], cost = edge[2];
+                if (cost <= t) {
+                    if (f[t - cost][j] != Integer.MAX_VALUE) {
+                        f[t][i] = Math.min(f[t][i], f[t - cost][j] + passingFees[i]);
+                    }
+                    if (f[t - cost][i] != Integer.MAX_VALUE) {
+                        f[t][j] = Math.min(f[t][j], f[t - cost][i] + passingFees[j]);
+                    }
+                }
+            }
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int t = 1; t <= maxTime; t++) {
+            ans = Math.min(ans, f[t][n - 1]);
+        }
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+}
---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202410/MinSpeedOnTime.java
@@ -0,0 +1,44 @@
+package cn.ccy.leetcode.archive_202410;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2024/10/2 22:04
+ * @description https://leetcode.cn/problems/minimum-speed-to-arrive-on-time/?envType=daily-question&envId=2024-10-02
+ */
+public class MinSpeedOnTime {
+    public static void main(String[] args) {
+
+    }
+
+    public int minSpeedOnTime(int[] dist, double hour) {
+        int n = dist.length;
+        // 将 hour 乘 100 以转为整数
+        long hr = Math.round(hour * 100);
+        // 时间必须要大于路程段数减 1
+        if (hr <= (n - 1) * 100) {
+            return -1;
+        }
+        // 二分
+        int l = 1;
+        int r = 10000000;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            // 判断当前时速是否满足时限
+            long t = 0;
+            // 前 n-1 段中第 i 段贡献的时间： floor(dist[i] / mid)
+            for (int i = 0; i < n - 1; ++i) {
+                t += (dist[i] - 1) / mid + 1;
+            }
+            // 最后一段贡献的时间： dist[n-1] / mid
+            t *= mid;
+            t += dist[n - 1];
+            if (t * 100 <= hr * mid) { // 通分以转化为整数比较
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l; // 满足条件的最小时速
+    }
+}
