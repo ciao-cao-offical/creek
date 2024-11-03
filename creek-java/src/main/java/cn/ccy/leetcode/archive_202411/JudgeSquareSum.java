c4de56ef7fa163a9aade121e2cc098997aa27b13
ciao <843078365@qq.com>
2024-11-04 00:40:45 +0800

    feat(creek-java):LeetCode commit 2024/11/04

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202411/JudgeSquareSum.java
@@ -0,0 +1,23 @@
+package cn.ccy.leetcode.archive_202411;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2024/11/4 00:39:46
+ * @description https://leetcode.cn/problems/sum-of-square-numbers/?envType=daily-question&envId=2024-11-04
+ */
+public class JudgeSquareSum {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean judgeSquareSum(int c) {
+        for (long a = 0; a * a <= c; a++) {
+            double b = Math.sqrt(c - a * a);
+            if (b == (int) b) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
