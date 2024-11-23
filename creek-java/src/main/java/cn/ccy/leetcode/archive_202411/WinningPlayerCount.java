b17cdfe8f00e2f43693dc8cd072f8e931725681e
ciao <843078365@qq.com>
2024-11-23 13:10:54 +0800

    feat(creek-java):LeetCode commit 2024/11/23

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202411/WinningPlayerCount.java
@@ -0,0 +1,30 @@
+package cn.ccy.leetcode.archive_202411;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2024/11/23 12:56:13
+ * @description https://leetcode.cn/problems/find-the-number-of-winning-players/?envType=daily-question&envId=2024-11-23
+ */
+public class WinningPlayerCount {
+    public static void main(String[] args) {
+
+    }
+
+    public int winningPlayerCount(int n, int[][] pick) {
+        int[][] cnt = new int[n][11];
+        for (int[] p : pick) {
+            cnt[p[0]][p[1]]++;
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= 10; j++) {
+                if (cnt[i][j] > i) {
+                    ans++;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
