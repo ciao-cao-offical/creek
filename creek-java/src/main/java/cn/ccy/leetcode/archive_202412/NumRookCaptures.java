ee0efbacc30bc7feec04d124969516ea84b47ea3
ciao <843078365@qq.com>
2024-12-06 00:45:51 +0800

    feat(creek-java):LeetCode commit 2024/12/06

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202412/NumRookCaptures.java
@@ -0,0 +1,42 @@
+package cn.ccy.leetcode.archive_202412;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2024/12/6 00:42:23
+ * @description https://leetcode.cn/problems/available-captures-for-rook/description/?envType=daily-question&envId=2024-12-06
+ */
+public class NumRookCaptures {
+    public static void main(String[] args) {
+
+    }
+
+    public int numRookCaptures(char[][] board) {
+        int cnt = 0, st = 0, ed = 0;
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                if (board[i][j] == 'R') {
+                    st = i;
+                    ed = j;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < 4; ++i) {
+            for (int step = 0; ; ++step) {
+                int tx = st + step * dx[i];
+                int ty = ed + step * dy[i];
+                if (tx < 0 || tx >= 8 || ty < 0 || ty >= 8 || board[tx][ty] == 'B') {
+                    break;
+                }
+                if (board[tx][ty] == 'p') {
+                    cnt++;
+                    break;
+                }
+            }
+        }
+        return cnt;
+    }
+}
