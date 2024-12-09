6a533e1c9240c81e2066f24e317b118070a17ad9
ciao <843078365@qq.com>
2024-12-10 00:26:14 +0800

    feat(creek-java):LeetCode commit 2024/12/10

---
+++ creek-java/src/main/java/cn/ccy/leetcode/archive_202412/SquareIsWhite.java
@@ -0,0 +1,17 @@
+package cn.ccy.leetcode.archive_202412;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2024/12/9 22:15:34
+ * @description https://leetcode.cn/problems/determine-color-of-a-chessboard-square/?envType=daily-question&envId=2024-12-09
+ */
+public class SquareIsWhite {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean squareIsWhite(String coordinates) {
+        return ((coordinates.charAt(0) - 'a' + 1) + (coordinates.charAt(1) - '0')) % 2 == 1;
+    }
+}
