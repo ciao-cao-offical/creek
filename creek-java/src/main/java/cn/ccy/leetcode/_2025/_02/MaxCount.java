dc497239c518b546800fd0351ddb873b341d57e0
ciao-cao-offical <843078365@qq.com>
2025-02-02 22:20:56 +0800

    feat(creek-java):LeetCode commit 2025/02/02

--- creek-java/src/main/java/cn/ccy/leetcode/_2025/_02/MaxCount.java
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_02/MaxCount.java
@@ -1,5 +1,23 @@
 package cn.ccy.leetcode._2025._02;
 
-public class MaxCount {
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2025/2/2 22:14:00
+ * @description https://leetcode.cn/problems/search-in-rotated-sorted-array-ii/?envType=daily-question&envId=2025-02-01
+ */
 
+public class MaxCount {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    public int maxCount(int m, int n, int[][] ops) {
+        int mina = m, minb = n;
+        for (int[] op : ops) {
+            mina = Math.min(mina, op[0]);
+            minb = Math.min(minb, op[1]);
+        }
+        return mina * minb;
+    }
 }
