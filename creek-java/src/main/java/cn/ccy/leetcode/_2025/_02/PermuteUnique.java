f04086d732062a0c20fe998d89bbe87cd0b492c5
ciao-cao-offical <843078365@qq.com>
2025-02-06 17:25:37 +0800

    feat(creek-java):LeetCode commit 2025/02/06

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_02/PermuteUnique.java
@@ -0,0 +1,46 @@
+package cn.ccy.leetcode._2025._02;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2025/2/6 13:31
+ * @description/link https://leetcode.cn/problems/permutations-ii/?envType=daily-question&envId=2025-02-06
+ * ☆☆☆ 2025cvd
+ */
+public class PermuteUnique {
+    public static void main(String[] args) {
+
+    }
+
+    boolean[] vis;
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> perm = new ArrayList<Integer>();
+        vis = new boolean[nums.length];
+        Arrays.sort(nums);
+        backtrack(nums, ans, 0, perm);
+        return ans;
+    }
+
+    public void backtrack(int[] nums, List<List<Integer>> ans, int idx, List<Integer> perm) {
+        if (idx == nums.length) {
+            ans.add(new ArrayList<Integer>(perm));
+            return;
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (vis[i] || (i > 0 && nums[i] == nums[i - 1] && !vis[i - 1])) {
+                continue;
+            }
+            perm.add(nums[i]);
+            vis[i] = true;
+            backtrack(nums, ans, idx + 1, perm);
+            vis[i] = false;
+            perm.remove(idx);
+        }
+    }
+}
