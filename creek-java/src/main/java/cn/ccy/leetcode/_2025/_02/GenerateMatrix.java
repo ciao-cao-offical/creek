3b92db34cf04aef9cc3d177e1a4252dbd32d720c
ciao-cao-offical <843078365@qq.com>
2025-02-07 21:35:52 +0800

    feat(creek-java):LeetCode commit 2025/02/07

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_02/GenerateMatrix.java
@@ -0,0 +1,33 @@
+package cn.ccy.leetcode._2025._02;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2025/2/7 21:21
+ * @description/link https://leetcode.cn/problems/spiral-matrix-ii/?envType=daily-question&envId=2025-02-07
+ */
+public class GenerateMatrix {
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] generateMatrix(int n) {
+        int maxNum = n * n;
+        int curNum = 1;
+        int[][] matrix = new int[n][n];
+        int row = 0, column = 0;
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // 右下左上
+        int directionIndex = 0;
+        while (curNum <= maxNum) {
+            matrix[row][column] = curNum;
+            curNum++;
+            int nextRow = row + directions[directionIndex][0], nextColumn = column + directions[directionIndex][1];
+            if (nextRow < 0 || nextRow >= n || nextColumn < 0 || nextColumn >= n || matrix[nextRow][nextColumn] != 0) {
+                directionIndex = (directionIndex + 1) % 4; // 顺时针旋转至下一个方向
+            }
+            row = row + directions[directionIndex][0];
+            column = column + directions[directionIndex][1];
+        }
+        return matrix;
+    }
+}
