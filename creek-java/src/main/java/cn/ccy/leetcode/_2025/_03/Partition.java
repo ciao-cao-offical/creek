3a1c98966cf707606dc05401a0051d9cb3eaad0b
ciao-cao-offical <843078365@qq.com>
2025-03-01 19:23:38 +0800

    feat(creek-java):LeetCode commit 2025/03/01

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_03/Partition.java
@@ -0,0 +1,53 @@
+package cn.ccy.leetcode._2025._03;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @date 2025/3/1 19:22
+ * @description/link https://leetcode.cn/problems/palindrome-partitioning/?envType=daily-question&envId=2025-03-01
+ */
+public class Partition {
+    public static void main(String[] args) {
+
+    }
+
+    boolean[][] f;
+    List<List<String>> ret = new ArrayList<List<String>>();
+    List<String> ans = new ArrayList<String>();
+    int n;
+
+    public List<List<String>> partition(String s) {
+        n = s.length();
+        f = new boolean[n][n];
+        for (int i = 0; i < n; ++i) {
+            Arrays.fill(f[i], true);
+        }
+
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                f[i][j] = (s.charAt(i) == s.charAt(j)) && f[i + 1][j - 1];
+            }
+        }
+
+        dfs(s, 0);
+        return ret;
+    }
+
+    public void dfs(String s, int i) {
+        if (i == n) {
+            ret.add(new ArrayList<String>(ans));
+            return;
+        }
+        for (int j = i; j < n; ++j) {
+            if (f[i][j]) {
+                ans.add(s.substring(i, j + 1));
+                dfs(s, j + 1);
+                ans.remove(ans.size() - 1);
+            }
+        }
+    }
+}
