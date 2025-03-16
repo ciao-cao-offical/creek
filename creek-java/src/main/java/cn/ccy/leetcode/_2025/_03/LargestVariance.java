121101f657983efae020115f104c75376a9adc90
ciao-cao-offical <843078365@qq.com>
2025-03-16 18:26:24 +0800

    feat(creek-java):LeetCode commit 2025/03/16

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_03/LargestVariance.java
@@ -0,0 +1,51 @@
+package cn.ccy.leetcode._2025._03;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/substring-with-largest-variance/?envType=daily-question&envId=2025-03-16">2272. 最大波动的子字符串</a>
+ * @since 2025/3/16 18:24
+ * ☆☆☆☆☆ 2025cvd
+ */
+public class LargestVariance {
+    public static void main(String[] args) {
+
+    }
+
+    public int largestVariance(String s) {
+        Map<Character, List<Integer>> pos = new HashMap<>();
+        for (int i = 0; i < s.length(); ++i) {
+            pos.computeIfAbsent(s.charAt(i), k -> new ArrayList<>()).add(i);
+        }
+
+        int ans = 0;
+        for (char c0 : pos.keySet()) {
+            for (char c1 : pos.keySet()) {
+                if (c0 != c1) {
+                    List<Integer> pos0 = pos.get(c0);
+                    List<Integer> pos1 = pos.get(c1);
+                    int i = 0, j = 0;
+                    int f = 0, g = Integer.MIN_VALUE;
+                    while (i < pos0.size() || j < pos1.size()) {
+                        if (j == pos1.size() || (i < pos0.size() && pos0.get(i) < pos1.get(j))) {
+                            f = Math.max(f, 0) + 1;
+                            g = g + 1;
+                            i++;
+                        } else {
+                            g = Math.max(Math.max(f, g), 0) - 1;
+                            f = Math.max(f, 0) - 1;
+                            j++;
+                        }
+                        ans = Math.max(ans, g);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
