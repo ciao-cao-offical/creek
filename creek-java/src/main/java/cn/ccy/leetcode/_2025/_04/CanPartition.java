831424a15c0a1a31f328038609f5e80e1edf15e1
ciao-cao-official <843078365@qq.com>
2025-04-07 20:36:59 +0800

    feat(creek-java):LeetCode commit 2025/04/07

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_04/CanPartition.java
@@ -0,0 +1,48 @@
+package cn.ccy.leetcode._2025._04;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/partition-equal-subset-sum/?envType=daily-question&envId=2025-04-07">416. 分割等和子集</a>
+ * @since 2025/4/7 20:35
+ */
+public class CanPartition {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return false;
+        }
+        int sum = 0, maxNum = 0;
+        for (int num : nums) {
+            sum += num;
+            maxNum = Math.max(maxNum, num);
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int target = sum / 2;
+        if (maxNum > target) {
+            return false;
+        }
+        boolean[][] dp = new boolean[n][target + 1];
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+        dp[0][nums[0]] = true;
+        for (int i = 1; i < n; i++) {
+            int num = nums[i];
+            for (int j = 1; j <= target; j++) {
+                if (j >= num) {
+                    dp[i][j] = dp[i - 1][j] | dp[i - 1][j - num];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n - 1][target];
+    }
+}
