0a1b5af89e79ad9e752e9560886fbf3c54570061
ciao-cao-offical <843078365@qq.com>
2025-05-18 22:29:49 +0800

    feat(creek-java):LeetCode commit 2025/05/18

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_05/ColorTheGrid.java
@@ -0,0 +1,82 @@
+package cn.ccy.leetcode._2025._05;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/painting-a-grid-with-three-different-colors/?envType=daily-question&envId=2025-05-18">1931. 用三种不同颜色为网格涂色</a>
+ * @since 2025/5/18 22:28
+ */
+public class ColorTheGrid {
+    public static void main(String[] args) {
+
+    }
+
+    static final int mod = 1000000007;
+
+    public int colorTheGrid(int m, int n) {
+        // 哈希映射 valid 存储所有满足要求的对一行进行涂色的方案
+        Map<Integer, List<Integer>> valid = new HashMap<>();
+        // 在 [0, 3^m) 范围内枚举满足要求的 mask
+        int maskEnd = (int) Math.pow(3, m);
+        for (int mask = 0; mask < maskEnd; ++mask) {
+            List<Integer> color = new ArrayList<>();
+            int mm = mask;
+            for (int i = 0; i < m; ++i) {
+                color.add(mm % 3);
+                mm /= 3;
+            }
+            boolean check = true;
+            for (int i = 0; i < m - 1; ++i) {
+                if (color.get(i).equals(color.get(i + 1))) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) {
+                valid.put(mask, color);
+            }
+        }
+
+        // 预处理所有的 (mask1, mask2) 二元组，满足 mask1 和 mask2 作为相邻行时，同一列上两个格子的颜色不同
+        Map<Integer, List<Integer>> adjacent = new HashMap<>();
+        for (int mask1 : valid.keySet()) {
+            for (int mask2 : valid.keySet()) {
+                boolean check = true;
+                for (int i = 0; i < m; ++i) {
+                    if (valid.get(mask1).get(i).equals(valid.get(mask2).get(i))) {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check) {
+                    adjacent.computeIfAbsent(mask1, k -> new ArrayList<>()).add(mask2);
+                }
+            }
+        }
+
+        Map<Integer, Integer> f = new HashMap<>();
+        for (int mask : valid.keySet()) {
+            f.put(mask, 1);
+        }
+        for (int i = 1; i < n; ++i) {
+            Map<Integer, Integer> g = new HashMap<>();
+            for (int mask2 : valid.keySet()) {
+                for (int mask1 : adjacent.getOrDefault(mask2, new ArrayList<>())) {
+                    g.put(mask2, (g.getOrDefault(mask2, 0) + f.getOrDefault(mask1, 0)) % mod);
+                }
+            }
+            f = g;
+        }
+
+        int ans = 0;
+        for (int num : f.values()) {
+            ans = (ans + num) % mod;
+        }
+        return ans;
+    }
+}
