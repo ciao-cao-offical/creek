61c3b78b32cad65c3ee07b6730a70037c3b60679
ciao-cao-offical <843078365@qq.com>
2025-05-20 23:46:57 +0800

    feat(creek-java):LeetCode commit 2025/05/20

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_05/IsZeroArray.java
@@ -0,0 +1,35 @@
+package cn.ccy.leetcode._2025._05;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/zero-array-transformation-i/?envType=daily-question&envId=2025-05-20">3355. 零数组变换 I</a>
+ * @since 2025/5/20 23:45
+ */
+public class IsZeroArray {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isZeroArray(int[] nums, int[][] queries) {
+        int[] deltaArray = new int[nums.length + 1];
+        for (int[] query : queries) {
+            int left = query[0];
+            int right = query[1];
+            deltaArray[left] += 1;
+            deltaArray[right + 1] -= 1;
+        }
+        int[] operationCounts = new int[deltaArray.length];
+        int currentOperations = 0;
+        for (int i = 0; i < deltaArray.length; i++) {
+            currentOperations += deltaArray[i];
+            operationCounts[i] = currentOperations;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (operationCounts[i] < nums[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
