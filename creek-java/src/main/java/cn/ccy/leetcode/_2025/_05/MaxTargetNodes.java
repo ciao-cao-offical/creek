3b437599cfb2c0037caf84b890e596ac36d6a87b
ciao-cao-offical <843078365@qq.com>
2025-05-28 22:51:29 +0800

    feat(creek-java):LeetCode commit 2025/05/28

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_05/MaxTargetNodes.java
@@ -0,0 +1,64 @@
+package cn.ccy.leetcode._2025._05;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/maximize-the-number-of-target-nodes-after-connecting-trees-i/?envType=daily-question&envId=2025-05-28">3372. 连接两棵树后最大目标节点数目 I</a>
+ * @since 2025/5/28 22:49
+ */
+public class MaxTargetNodes {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] maxTargetNodes(int[][] edges1, int[][] edges2, int k) {
+        int n = edges1.length + 1, m = edges2.length + 1;
+        int[] count1 = build(edges1, k);
+        int[] count2 = build(edges2, k - 1);
+        int maxCount2 = 0;
+        for (int c : count2) {
+            if (c > maxCount2) {
+                maxCount2 = c;
+            }
+        }
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = count1[i] + maxCount2;
+        }
+        return res;
+    }
+
+    private int[] build(int[][] edges, int k) {
+        int n = edges.length + 1;
+        List<List<Integer>> children = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            children.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            children.get(edge[0]).add(edge[1]);
+            children.get(edge[1]).add(edge[0]);
+        }
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = dfs(i, -1, children, k);
+        }
+        return res;
+    }
+
+    private int dfs(int node, int parent, List<List<Integer>> children, int k) {
+        if (k < 0) {
+            return 0;
+        }
+        int res = 1;
+        for (int child : children.get(node)) {
+            if (child == parent) {
+                continue;
+            }
+            res += dfs(child, node, children, k - 1);
+        }
+        return res;
+    }
+}
