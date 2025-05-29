f119abd87f75cb0140807c791dd607ed112fafb2
ciao-cao-offical <843078365@qq.com>
2025-05-29 22:38:35 +0800

    feat(creek-java):LeetCode commit 2025/05/29

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_05/MaxTargetNodes2.java
@@ -0,0 +1,56 @@
+package cn.ccy.leetcode._2025._05;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/maximize-the-number-of-target-nodes-after-connecting-trees-ii/?envType=daily-question&envId=2025-05-29">3373. 连接两棵树后最大目标节点数目 II</a>
+ * @since 2025/5/29 22:27
+ * ★★★★★ 2025cvd
+ */
+public class MaxTargetNodes2 {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] maxTargetNodes(int[][] edges1, int[][] edges2) {
+        int n = edges1.length + 1, m = edges2.length + 1;
+        int[] color1 = new int[n];
+        int[] color2 = new int[m];
+        int[] count1 = build(edges1, color1);
+        int[] count2 = build(edges2, color2);
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = count1[color1[i]] + Math.max(count2[0], count2[1]);
+        }
+        return res;
+    }
+
+    private int[] build(int[][] edges, int[] color) {
+        int n = edges.length + 1;
+        List<List<Integer>> children = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            children.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            children.get(edge[0]).add(edge[1]);
+            children.get(edge[1]).add(edge[0]);
+        }
+        int res = dfs(0, -1, 0, children, color);
+        return new int[]{res, n - res};
+    }
+
+    private int dfs(int node, int parent, int depth, List<List<Integer>> children, int[] color) {
+        int res = 1 - depth % 2;
+        color[node] = depth % 2;
+        for (int child : children.get(node)) {
+            if (child == parent) {
+                continue;
+            }
+            res += dfs(child, node, depth + 1, children, color);
+        }
+        return res;
+    }
+}
