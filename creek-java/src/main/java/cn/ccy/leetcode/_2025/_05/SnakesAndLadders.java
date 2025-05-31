3e5881d3d80d107cc157c46013e026afec907faa
ciao-cao-offical <843078365@qq.com>
2025-05-31 16:35:15 +0800

    feat(creek-java):LeetCode commit 2025/05/31

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_05/SnakesAndLadders.java
@@ -0,0 +1,52 @@
+package cn.ccy.leetcode._2025._05;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/snakes-and-ladders/?envType=daily-question&envId=2025-05-31">909. 蛇梯棋</a>
+ * @since 2025/5/31 16:32
+ */
+public class SnakesAndLadders {
+    public static void main(String[] args) {
+
+    }
+
+    public int snakesAndLadders(int[][] board) {
+        int n = board.length;
+        boolean[] vis = new boolean[n * n + 1];
+        Queue<int[]> queue = new LinkedList<int[]>();
+        queue.offer(new int[]{1, 0});
+        while (!queue.isEmpty()) {
+            int[] p = queue.poll();
+            for (int i = 1; i <= 6; ++i) {
+                int nxt = p[0] + i;
+                if (nxt > n * n) { // 超出边界
+                    break;
+                }
+                int[] rc = id2rc(nxt, n); // 得到下一步的行列
+                if (board[rc[0]][rc[1]] > 0) { // 存在蛇或梯子
+                    nxt = board[rc[0]][rc[1]];
+                }
+                if (nxt == n * n) { // 到达终点
+                    return p[1] + 1;
+                }
+                if (!vis[nxt]) {
+                    vis[nxt] = true;
+                    queue.offer(new int[]{nxt, p[1] + 1}); // 扩展新状态
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int[] id2rc(int id, int n) {
+        int r = (id - 1) / n, c = (id - 1) % n;
+        if (r % 2 == 1) {
+            c = n - 1 - c;
+        }
+        return new int[]{n - 1 - r, c};
+    }
+}
