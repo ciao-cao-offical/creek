20debfbfb46784be630c753e1287b55c4b3c13f0
ciao-cao-offical <843078365@qq.com>
2025-07-06 17:23:06 +0800

    feat(creek-java):LeetCode commit 2025/07/06

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_07/FindSumPairs.java
@@ -0,0 +1,45 @@
+package cn.ccy.leetcode._2025._07;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/finding-pairs-with-a-certain-sum/?envType=daily-question&envId=2025-07-06">...</a>
+ * @since 2025/7/6 17:20
+ */
+public class FindSumPairs {
+    public static void main(String[] args) {
+
+    }
+
+    private int[] nums1;
+    private int[] nums2;
+    private Map<Integer, Integer> cnt;
+
+    public FindSumPairs(int[] nums1, int[] nums2) {
+        this.nums1 = nums1;
+        this.nums2 = nums2;
+        this.cnt = new HashMap<>();
+        for (int num : nums2) {
+            cnt.put(num, cnt.getOrDefault(num, 0) + 1);
+        }
+    }
+
+    public void add(int index, int val) {
+        int oldVal = nums2[index];
+        cnt.put(oldVal, cnt.get(oldVal) - 1);
+        nums2[index] += val;
+        cnt.put(nums2[index], cnt.getOrDefault(nums2[index], 0) + 1);
+    }
+
+    public int count(int tot) {
+        int ans = 0;
+        for (int num : nums1) {
+            int rest = tot - num;
+            ans += cnt.getOrDefault(rest, 0);
+        }
+        return ans;
+    }
+}
