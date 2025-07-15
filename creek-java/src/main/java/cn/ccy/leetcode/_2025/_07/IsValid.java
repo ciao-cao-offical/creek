d05ac664d61ce6d12f24020742a7f2230084b30a
ciao-cao-offical <843078365@qq.com>
2025-07-15 22:31:32 +0800

    feat(creek-java):LeetCode commit 2025/07/15

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_07/IsValid.java
@@ -0,0 +1,34 @@
+package cn.ccy.leetcode._2025._07;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/valid-word/?envType=daily-question&envId=2025-07-15">3136. 有效单词</a>
+ * @since 2025/7/15 22:23
+ */
+public class IsValid {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isValid(String word) {
+        if (word.length() < 3) {
+            return false;
+        }
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+        for (char c : word.toCharArray()) {
+            if (Character.isLetter(c)) {
+                char ch = Character.toLowerCase(c);
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            } else if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return hasVowel && hasConsonant;
+    }
+}
