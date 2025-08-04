735d8eb6354085bc401c677941aab767e629c775
ciao-cao-offical <843078365@qq.com>
2025-08-04 23:17:32 +0800

    feat(creek-java):LeetCode commit 2025/08/04

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_08/TotalFruit.java
@@ -0,0 +1,35 @@
+package cn.ccy.leetcode._2025._08;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/fruit-into-baskets/?envType=daily-question&envId=2025-08-04">904. 水果成篮</a>
+ * @since 2025/8/4 23:15
+ */
+public class TotalFruit {
+    public static void main(String[] args) {
+
+    }
+
+    public int totalFruit(int[] fruits) {
+        int n = fruits.length;
+        Map<Integer, Integer> cnt = new HashMap<Integer, Integer>();
+
+        int left = 0, ans = 0;
+        for (int right = 0; right < n; ++right) {
+            cnt.put(fruits[right], cnt.getOrDefault(fruits[right], 0) + 1);
+            while (cnt.size() > 2) {
+                cnt.put(fruits[left], cnt.get(fruits[left]) - 1);
+                if (cnt.get(fruits[left]) == 0) {
+                    cnt.remove(fruits[left]);
+                }
+                ++left;
+            }
+            ans = Math.max(ans, right - left + 1);
+        }
+        return ans;
+    }
+}
