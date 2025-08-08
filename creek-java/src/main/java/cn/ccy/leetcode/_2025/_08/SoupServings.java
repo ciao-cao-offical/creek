8b778a48658737eb40228c655bcf357bd56a473f
ciao-cao-offical <843078365@qq.com>
2025-08-08 22:43:38 +0800

    feat(creek-java):LeetCode commit 2025/08/08

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_08/SoupServings.java
@@ -0,0 +1,31 @@
+package cn.ccy.leetcode._2025._08;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/soup-servings/?envType=daily-question&envId=2025-08-08">808. 分汤</a>
+ * @since 2025/8/8 22:41
+ */
+public class SoupServings {
+    public static void main(String[] args) {
+
+    }
+
+    public double soupServings(int n) {
+        n = (int) Math.ceil((double) n / 25);
+        if (n >= 179) {
+            return 1.0;
+        }
+        double[][] dp = new double[n + 1][n + 1];
+        dp[0][0] = 0.5;
+        for (int i = 1; i <= n; i++) {
+            dp[0][i] = 1.0;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = (dp[Math.max(0, i - 4)][j] + dp[Math.max(0, i - 3)][Math.max(0, j - 1)] + dp[Math.max(0, i - 2)][Math.max(0, j - 2)] + dp[Math.max(0, i - 1)][Math.max(0, j - 3)]) / 4.0;
+            }
+        }
+        return dp[n][n];
+    }
+}
