d9b8952be5e7e0f156d0793763e43d77f1a61b9b
ciao-cao-offical <843078365@qq.com>
2025-08-17 23:36:05 +0800

    feat(creek-java):LeetCode commit 2025/08/17

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_08/New21Game.java
@@ -0,0 +1,29 @@
+package cn.ccy.leetcode._2025._08;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/new-21-game/?envType=daily-question&envId=2025-08-17">837. 新 21 点</a>
+ * @since 2025/8/17 23:23
+ */
+public class New21Game {
+    public static void main(String[] args) {
+
+    }
+
+    public double new21Game(int n, int k, int maxPts) {
+        if (k == 0) {
+            return 1.0;
+        }
+        double[] dp = new double[k + maxPts];
+        for (int i = k; i <= n && i < k + maxPts; i++) {
+            dp[i] = 1.0;
+        }
+        for (int i = k - 1; i >= 0; i--) {
+            for (int j = 1; j <= maxPts; j++) {
+                dp[i] += dp[i + j] / maxPts;
+            }
+        }
+        return dp[0];
+    }
+}
