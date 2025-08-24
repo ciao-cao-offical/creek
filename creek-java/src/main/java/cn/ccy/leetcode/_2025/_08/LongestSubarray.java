39494aa7fb1476a6298af16d45bf465829427d68
ciao-cao-offical <843078365@qq.com>
2025-08-24 21:31:22 +0800

    feat(creek-java):LeetCode commit 2025/08/24

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_08/LongestSubarray.java
@@ -0,0 +1,39 @@
+package cn.ccy.leetcode._2025._08;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/longest-subarray-of-1s-after-deleting-one-element/?envType=daily-question&envId=2025-08-24">1493. 删掉一个元素以后全为 1 的最长子数组</a>
+ * @since 2025/8/24 21:29
+ */
+public class LongestSubarray {
+    public static void main(String[] args) {
+
+    }
+
+    public int longestSubarray(int[] nums) {
+        int n = nums.length;
+
+        int[] pre = new int[n];
+        int[] suf = new int[n];
+
+        pre[0] = nums[0];
+        for (int i = 1; i < n; ++i) {
+            pre[i] = nums[i] != 0 ? pre[i - 1] + 1 : 0;
+        }
+
+        suf[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            suf[i] = nums[i] != 0 ? suf[i + 1] + 1 : 0;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int preSum = i == 0 ? 0 : pre[i - 1];
+            int sufSum = i == n - 1 ? 0 : suf[i + 1];
+            ans = Math.max(ans, preSum + sufSum);
+        }
+
+        return ans;
+    }
+}
