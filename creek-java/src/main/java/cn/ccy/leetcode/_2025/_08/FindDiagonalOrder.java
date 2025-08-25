5c57db4f1bf7df400c79365bc475d1fa745b907d
ciao-cao-offical <843078365@qq.com>
2025-08-25 23:48:27 +0800

    feat(creek-java):LeetCode commit 2025/08/25

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_08/FindDiagonalOrder.java
@@ -0,0 +1,42 @@
+package cn.ccy.leetcode._2025._08;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/diagonal-traverse/?envType=daily-question&envId=2025-08-25">498. 对角线遍历</a>
+ * @since 2025/8/25 23:46
+ */
+public class FindDiagonalOrder {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] findDiagonalOrder(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] res = new int[m * n];
+        int pos = 0;
+        for (int i = 0; i < m + n - 1; i++) {
+            if (i % 2 == 1) {
+                int x = i < n ? 0 : i - n + 1;
+                int y = i < n ? i : n - 1;
+                while (x < m && y >= 0) {
+                    res[pos] = mat[x][y];
+                    pos++;
+                    x++;
+                    y--;
+                }
+            } else {
+                int x = i < m ? i : m - 1;
+                int y = i < m ? 0 : i - m + 1;
+                while (x >= 0 && y < n) {
+                    res[pos] = mat[x][y];
+                    pos++;
+                    x--;
+                    y++;
+                }
+            }
+        }
+        return res;
+    }
+}
