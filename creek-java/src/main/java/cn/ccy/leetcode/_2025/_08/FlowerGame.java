449cd81f80d5353517d27443fd699a723ccf20a7
ciao-cao-offical <843078365@qq.com>
2025-08-29 23:41:47 +0800

    feat(creek-java):LeetCode commit 2025/08/29

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_08/FlowerGame.java
@@ -0,0 +1,17 @@
+package cn.ccy.leetcode._2025._08;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/alice-and-bob-playing-flower-game/?envType=daily-question&envId=2025-08-29">3021. Alice 和 Bob 玩鲜花游戏</a>
+ * @since 2025/8/29 23:34
+ */
+public class FlowerGame {
+    public static void main(String[] args) {
+
+    }
+
+    public long flowerGame(int n, int m) {
+        return (long) m * n / 2;
+    }
+}
