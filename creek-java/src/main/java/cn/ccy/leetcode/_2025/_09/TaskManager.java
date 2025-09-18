b841d3c5f49df6c3c545bd2fd087c3d5a154a96c
ciao-cao-offical <843078365@qq.com>
2025-09-18 23:35:27 +0800

    feat(creek-java):LeetCode commit 2025/09/18

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_09/TaskManager.java
@@ -0,0 +1,67 @@
+package cn.ccy.leetcode._2025._09;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/design-task-manager/?envType=daily-question&envId=2025-09-18">3408. 设计任务管理器</a>
+ * @since 2025/9/18 23:31
+ */
+public class TaskManager {
+    public static void main(String[] args) {
+
+    }
+
+    private Map<Integer, int[]> taskInfo;
+    private PriorityQueue<int[]> heap;
+
+    public TaskManager(List<List<Integer>> tasks) {
+        taskInfo = new HashMap<>();
+        heap = new PriorityQueue<>((a, b) -> {
+            if (a[0] != b[0]) {
+                return b[0] - a[0];
+            }
+            return b[1] - a[1];
+        });
+
+        for (List<Integer> task : tasks) {
+            int userId = task.get(0), taskId = task.get(1), priority = task.get(2);
+            taskInfo.put(taskId, new int[]{priority, userId});
+            heap.offer(new int[]{priority, taskId});
+        }
+    }
+
+    public void add(int userId, int taskId, int priority) {
+        taskInfo.put(taskId, new int[]{priority, userId});
+        heap.offer(new int[]{priority, taskId});
+    }
+
+    public void edit(int taskId, int newPriority) {
+        if (taskInfo.containsKey(taskId)) {
+            taskInfo.get(taskId)[0] = newPriority;
+            heap.offer(new int[]{newPriority, taskId});
+        }
+    }
+
+    public void rmv(int taskId) {
+        taskInfo.remove(taskId);
+    }
+
+    public int execTop() {
+        while (!heap.isEmpty()) {
+            int[] task = heap.poll();
+            int priority = task[0], taskId = task[1];
+
+            if (taskInfo.containsKey(taskId) && taskInfo.get(taskId)[0] == priority) {
+                int userId = taskInfo.get(taskId)[1];
+                taskInfo.remove(taskId);
+                return userId;
+            }
+        }
+        return -1;
+    }
+}
