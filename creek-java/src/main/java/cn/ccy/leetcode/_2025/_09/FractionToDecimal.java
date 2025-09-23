7e51c3b448d3c983140f5e86be1dc781646b88db
ciao-cao-offical <843078365@qq.com>
2025-09-24 00:37:35 +0800

    feat(creek-java):LeetCode commit 2025/09/24

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_09/FractionToDecimal.java
@@ -0,0 +1,57 @@
+package cn.ccy.leetcode._2025._09;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/fraction-to-recurring-decimal/?envType=daily-question&envId=2025-09-24">166. 分数到小数</a>
+ * @since 2025/9/24 00:36
+ */
+public class FractionToDecimal {
+    public static void main(String[] args) {
+
+    }
+
+    public String fractionToDecimal(int numerator, int denominator) {
+        long numeratorLong = (long) numerator;
+        long denominatorLong = (long) denominator;
+        if (numeratorLong % denominatorLong == 0) {
+            return String.valueOf(numeratorLong / denominatorLong);
+        }
+
+        StringBuffer sb = new StringBuffer();
+        if (numeratorLong < 0 ^ denominatorLong < 0) {
+            sb.append('-');
+        }
+
+        // 整数部分
+        numeratorLong = Math.abs(numeratorLong);
+        denominatorLong = Math.abs(denominatorLong);
+        long integerPart = numeratorLong / denominatorLong;
+        sb.append(integerPart);
+        sb.append('.');
+
+        // 小数部分
+        StringBuffer fractionPart = new StringBuffer();
+        Map<Long, Integer> remainderIndexMap = new HashMap<Long, Integer>();
+        long remainder = numeratorLong % denominatorLong;
+        int index = 0;
+        while (remainder != 0 && !remainderIndexMap.containsKey(remainder)) {
+            remainderIndexMap.put(remainder, index);
+            remainder *= 10;
+            fractionPart.append(remainder / denominatorLong);
+            remainder %= denominatorLong;
+            index++;
+        }
+        if (remainder != 0) { // 有循环节
+            int insertIndex = remainderIndexMap.get(remainder);
+            fractionPart.insert(insertIndex, '(');
+            fractionPart.append(')');
+        }
+        sb.append(fractionPart.toString());
+
+        return sb.toString();
+    }
+}
