8d810c028acb743736eb6d4ea24de3ea604741b2
ciao-cao-offical <843078365@qq.com>
2025-09-29 22:54:16 +0800

    feat(creek-java):LeetCode commit 2025/09/29

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_09/MinScoreTriangulation.java
@@ -0,0 +1,44 @@
+package cn.ccy.leetcode._2025._09;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/minimum-score-triangulation-of-polygon/?envType=daily-question&envId=2025-09-29">1039. 多边形三角剖分的最低得分</a>
+ * @since 2025/9/29 22:50
+ */
+public class MinScoreTriangulation {
+    public static void main(String[] args) {
+
+    }
+
+    int n;
+    int[] values;
+    Map<Integer, Integer> memo = new HashMap<Integer, Integer>();
+
+    public int minScoreTriangulation(int[] values) {
+        this.n = values.length;
+        this.values = values;
+        return dp(0, n - 1);
+    }
+
+    public int dp(int i, int j) {
+        if (i + 2 > j) {
+            return 0;
+        }
+        if (i + 2 == j) {
+            return values[i] * values[i + 1] * values[j];
+        }
+        int key = i * n + j;
+        if (!memo.containsKey(key)) {
+            int minScore = Integer.MAX_VALUE;
+            for (int k = i + 1; k < j; k++) {
+                minScore = Math.min(minScore, values[i] * values[k] * values[j] + dp(i, k) + dp(k, j));
+            }
+            memo.put(key, minScore);
+        }
+        return memo.get(key);
+    }
+}
