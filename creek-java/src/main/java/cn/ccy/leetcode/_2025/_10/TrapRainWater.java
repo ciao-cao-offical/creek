05796d0285ad48823b4312a4f8edf87a8ff66343
ciao-cao-offical <843078365@qq.com>
2025-10-03 23:24:05 +0800

    feat(creek-java):LeetCode commit 2025/10/03

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_10/TrapRainWater.java
@@ -0,0 +1,52 @@
+package cn.ccy.leetcode._2025._10;
+
+import java.util.PriorityQueue;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/trapping-rain-water-ii/?envType=daily-question&envId=2025-10-03">407. 接雨水 II</a>
+ * @since 2025/10/3 23:21
+ * ★★★★★ 2025cvd
+ */
+public class TrapRainWater {
+    public static void main(String[] args) {
+
+    }
+
+    public int trapRainWater(int[][] heightMap) {
+        if (heightMap.length <= 2 || heightMap[0].length <= 2) {
+            return 0;
+        }
+        int m = heightMap.length;
+        int n = heightMap[0].length;
+        boolean[][] visit = new boolean[m][n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 || i == m - 1 || j == 0 || j == n - 1) {
+                    pq.offer(new int[]{i * n + j, heightMap[i][j]});
+                    visit[i][j] = true;
+                }
+            }
+        }
+        int res = 0;
+        int[] dirs = {-1, 0, 1, 0, -1};
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            for (int k = 0; k < 4; ++k) {
+                int nx = curr[0] / n + dirs[k];
+                int ny = curr[0] % n + dirs[k + 1];
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visit[nx][ny]) {
+                    if (curr[1] > heightMap[nx][ny]) {
+                        res += curr[1] - heightMap[nx][ny];
+                    }
+                    pq.offer(new int[]{nx * n + ny, Math.max(heightMap[nx][ny], curr[1])});
+                    visit[nx][ny] = true;
+                }
+            }
+        }
+        return res;
+    }
+}
