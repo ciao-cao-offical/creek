93fce4d21c808707c8f8b9d0ea8c67e79d91ac3e
ciao-cao-offical <843078365@qq.com>
2025-10-17 00:11:23 +0800

    feat(creek-java):LeetCode commit 2025/10/17

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_10/MaxPartitionsAfterOperations.java
@@ -0,0 +1,71 @@
+package cn.ccy.leetcode._2025._10;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/maximize-the-number-of-partitions-after-operations/?envType=daily-question&envId=2025-10-17">3003. 执行操作后的最大分割数量</a>
+ * @since 2025/10/17 00:10
+ */
+public class MaxPartitionsAfterOperations {
+    public static void main(String[] args) {
+
+    }
+
+    public int maxPartitionsAfterOperations(String s, int k) {
+        int n = s.length();
+        int[][] left = new int[n][3];
+        int[][] right = new int[n][3];
+
+        int num = 0, mask = 0, count = 0;
+        for (int i = 0; i < n - 1; i++) {
+            int binary = 1 << (s.charAt(i) - 'a');
+            if ((mask & binary) == 0) {
+                count++;
+                if (count <= k) {
+                    mask |= binary;
+                } else {
+                    num++;
+                    mask = binary;
+                    count = 1;
+                }
+            }
+            left[i + 1][0] = num;
+            left[i + 1][1] = mask;
+            left[i + 1][2] = count;
+        }
+
+        num = 0;
+        mask = 0;
+        count = 0;
+        for (int i = n - 1; i > 0; i--) {
+            int binary = 1 << (s.charAt(i) - 'a');
+            if ((mask & binary) == 0) {
+                count++;
+                if (count <= k) {
+                    mask |= binary;
+                } else {
+                    num++;
+                    mask = binary;
+                    count = 1;
+                }
+            }
+            right[i - 1][0] = num;
+            right[i - 1][1] = mask;
+            right[i - 1][2] = count;
+        }
+
+        int maxVal = 0;
+        for (int i = 0; i < n; i++) {
+            int seg = left[i][0] + right[i][0] + 2;
+            int totMask = left[i][1] | right[i][1];
+            int totCount = Integer.bitCount(totMask);
+            if (left[i][2] == k && right[i][2] == k && totCount < 26) {
+                seg++;
+            } else if (Math.min(totCount + 1, 26) <= k) {
+                seg--;
+            }
+            maxVal = Math.max(maxVal, seg);
+        }
+        return maxVal;
+    }
+}
