17f1fea312b265bad76715ab88a166241c0211af
ciao-cao-offical <843078365@qq.com>
2025-11-11 00:11:53 +0800

    feat(creek-java):LeetCode commit 2025/11/11

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_11/FindMaxForm.java
@@ -0,0 +1,40 @@
+package cn.ccy.leetcode._2025._11;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/ones-and-zeroes/?envType=daily-question&envId=2025-11-11">474. 一和零</a>
+ * @since 2025/11/11 00:10
+ */
+public class FindMaxForm {
+    public static void main(String[] args) {
+
+    }
+
+    public int findMaxForm(String[] strs, int m, int n) {
+        int length = strs.length;
+        int[][][] dp = new int[length + 1][m + 1][n + 1];
+        for (int i = 1; i <= length; i++) {
+            int[] zerosOnes = getZerosOnes(strs[i - 1]);
+            int zeros = zerosOnes[0], ones = zerosOnes[1];
+            for (int j = 0; j <= m; j++) {
+                for (int k = 0; k <= n; k++) {
+                    dp[i][j][k] = dp[i - 1][j][k];
+                    if (j >= zeros && k >= ones) {
+                        dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][j - zeros][k - ones] + 1);
+                    }
+                }
+            }
+        }
+        return dp[length][m][n];
+    }
+
+    public int[] getZerosOnes(String str) {
+        int[] zerosOnes = new int[2];
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
+            zerosOnes[str.charAt(i) - '0']++;
+        }
+        return zerosOnes;
+    }
+}
