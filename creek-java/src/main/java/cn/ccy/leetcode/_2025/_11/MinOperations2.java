a093e50e6a4daaeee8a5d7f42677f8ec98420064
ciao-cao-offical <843078365@qq.com>
2025-11-12 00:14:24 +0800

    feat(creek-java):LeetCode commit 2025/11/12

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_11/MinOperations2.java
@@ -0,0 +1,53 @@
+package cn.ccy.leetcode._2025._11;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/minimum-number-of-operations-to-make-all-array-elements-equal-to-1/?envType=daily-question&envId=2025-11-12">2654. 使数组所有元素变成 1 的最少操作次数</a>
+ * @since 2025/11/12 00:11
+ */
+public class MinOperations2 {
+    public static void main(String[] args) {
+
+    }
+
+    public int minOperations(int[] nums) {
+        int n = nums.length;
+        int num1 = 0;
+        int g = 0;
+        for (int x : nums) {
+            if (x == 1) {
+                num1++;
+            }
+            g = gcd(g, x);
+        }
+        if (num1 > 0) {
+            return n - num1;
+        }
+        if (g > 1) {
+            return -1;
+        }
+
+        int minLen = n;
+        for (int i = 0; i < n; i++) {
+            int currentGcd = 0;
+            for (int j = i; j < n; j++) {
+                currentGcd = gcd(currentGcd, nums[j]);
+                if (currentGcd == 1) {
+                    minLen = Math.min(minLen, j - i + 1);
+                    break;
+                }
+            }
+        }
+        return minLen + n - 2;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
