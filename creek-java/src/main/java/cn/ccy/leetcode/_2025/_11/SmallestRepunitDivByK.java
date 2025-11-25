0f2a651d14945ebc9947dad86835bf1cb2cb0880
ciao-cao-official <843078365@qq.com>
2025-11-25 22:44:55 +0800

    feat(creek-java):LeetCode commit 2025/11/25

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_11/SmallestRepunitDivByK.java
@@ -0,0 +1,32 @@
+package cn.ccy.leetcode._2025._11;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/smallest-integer-divisible-by-k/?envType=daily-question&envId=2025-11-25">1015. 可被 K 整除的最小整数</a>
+ * @since 2025/11/25 22:43
+ */
+public class SmallestRepunitDivByK {
+    public static void main(String[] args) {
+
+    }
+
+    public int smallestRepunitDivByK(int k) {
+        int resid = 1 % k, len = 1; // resid为余数，len为数字长度，初始值为1
+        Set<Integer> set = new HashSet<Integer>(); // 创建一个无序集合，用于存储余数
+        set.add(resid); // 插入余数1
+        while (resid != 0) { // 当余数为0时退出循环
+            resid = (resid * 10 + 1) % k; // 计算下一个余数
+            len++; // 数字长度+1
+            if (set.contains(resid)) { // 如果余数重复出现，则无解
+                return -1;
+            }
+            set.add(resid); // 将余数插入集合
+        }
+        return len; // 返回数字长度
+    }
+}
