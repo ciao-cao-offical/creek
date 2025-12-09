07885f624ec7598909ecb19d720be8ebb8ba6371
ciao-cao-offical <843078365@qq.com>
2025-12-09 23:23:43 +0800

    feat(creek-java):LeetCode commit 2025/12/09

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2025/_12/SpecialTriplets.java
@@ -0,0 +1,37 @@
+package cn.ccy.leetcode._2025._12;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/count-special-triplets/?envType=daily-question&envId=2025-12-09">3583. 统计特殊三元组</a>
+ * @since 2025/12/9 23:22
+ */
+public class SpecialTriplets {
+    public static void main(String[] args) {
+
+    }
+
+    public int specialTriplets(int[] nums) {
+        final int MOD = 1000000007;
+        Map<Integer, Integer> numCnt = new HashMap<>();
+        Map<Integer, Integer> numPartialCnt = new HashMap<>();
+
+        for (int v : nums) {
+            numCnt.put(v, numCnt.getOrDefault(v, 0) + 1);
+        }
+
+        long ans = 0;
+        for (int v : nums) {
+            int target = v * 2;
+            int lCnt = numPartialCnt.getOrDefault(target, 0);
+            numPartialCnt.put(v, numPartialCnt.getOrDefault(v, 0) + 1);
+            int rCnt = numCnt.getOrDefault(target, 0) - numPartialCnt.getOrDefault(target, 0);
+            ans = (ans + (long) lCnt * rCnt) % MOD;
+        }
+
+        return (int) ans;
+    }
+}
