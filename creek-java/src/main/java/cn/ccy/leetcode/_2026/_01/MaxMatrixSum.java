c1cc9538f004eb08b68a60e771579781aff9d262
ciao-cao-offical <843078365@qq.com>
2026-01-05 01:16:52 +0800

    feat(creek-java):LeetCode commit 2026/01/05

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2026/_01/MaxMatrixSum.java
@@ -0,0 +1,35 @@
+package cn.ccy.leetcode._2026._01;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/maximum-matrix-sum/?envType=daily-question&envId=2026-01-05">1975. 最大方阵和</a>
+ * @since 2026/1/5 01:15
+ */
+public class MaxMatrixSum {
+    public static void main(String[] args) {
+
+    }
+
+    public long maxMatrixSum(int[][] matrix) {
+        int n = matrix.length;
+        int cnt = 0;   // 负数元素的数量
+        long total = 0;   // 所有元素的绝对值之和
+        int mn = Integer.MAX_VALUE;   // 方阵元素的最小绝对值
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                mn = Math.min(mn, Math.abs(matrix[i][j]));
+                if (matrix[i][j] < 0) {
+                    ++cnt;
+                }
+                total += Math.abs(matrix[i][j]);
+            }
+        }
+        // 按照负数元素的数量的奇偶性讨论
+        if (cnt % 2 == 0) {
+            return total;
+        } else {
+            return total - 2 * mn;
+        }
+    }
+}
