bef8ed145f509703b32d901fafc67e21ac88b9f5
ciao-cao-offical <843078365@qq.com>
2026-02-12 00:08:04 +0800

    feat(creek-java):LeetCode commit 2026/02/12

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2026/_02/LongestBalanced3.java
@@ -0,0 +1,42 @@
+package cn.ccy.leetcode._2026._02;
+
+import java.util.Arrays;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="">...</a>
+ * @since 2026/2/12 00:05
+ */
+public class LongestBalanced3 {
+    public static void main(String[] args) {
+
+    }
+
+    public int longestBalanced(String s) {
+        int n = s.length();
+        int res = 0;
+        int[] cnt = new int[26];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(cnt, 0);
+            for (int j = i; j < n; j++) {
+                boolean flag = true;
+                int c = s.charAt(j) - 'a';
+                cnt[c]++;
+
+                for (int x : cnt) {
+                    if (x > 0 && x != cnt[c]) {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag) {
+                    res = Math.max(res, j - i + 1);
+                }
+            }
+        }
+        return res;
+    }
+}
