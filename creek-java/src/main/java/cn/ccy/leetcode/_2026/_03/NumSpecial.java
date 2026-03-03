cf6b80d034c34546c274cd52eea0271a34f0b4e5
ciao-cao-offical <843078365@qq.com>
2026-03-04 01:22:04 +0800

    feat(creek-java):LeetCode commit 2026/03/04

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2026/_03/NumSpecial.java
@@ -0,0 +1,34 @@
+package cn.ccy.leetcode._2026._03;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/special-positions-in-a-binary-matrix/?envType=daily-question&envId=2026-03-04">1582. 二进制矩阵中的特殊位置</a>
+ * @since 2026/3/4 01:20
+ */
+public class NumSpecial {
+    public static void main(String[] args) {
+
+    }
+
+    public int numSpecial(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        int[] rowsSum = new int[m];
+        int[] colsSum = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                rowsSum[i] += mat[i][j];
+                colsSum[j] += mat[i][j];
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1 && rowsSum[i] == 1 && colsSum[j] == 1) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+}
