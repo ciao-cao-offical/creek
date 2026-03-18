6a039dc296cc8a65b5f32b7ac705b95d183144f6
ciao-cao-offical <843078365@qq.com>
2026-03-19 00:17:57 +0800

    feat(creek-java):LeetCode commit 2026/03/19

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2026/_03/NumberOfSubmatrices.java
@@ -0,0 +1,38 @@
+package cn.ccy.leetcode._2026._03;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/count-submatrices-with-equal-frequency-of-x-and-y/?envType=daily-question&envId=2026-03-19">3212. 统计 X 和 Y 频数相等的子矩阵数量</a>
+ * @since 2026/3/19 00:16
+ */
+public class NumberOfSubmatrices {
+    public static void main(String[] args) {
+
+    }
+
+    public int numberOfSubmatrices(char[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        int ans = 0;
+        int[][][] sum = new int[n + 1][m + 1][2];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 'X') {
+                    sum[i + 1][j + 1][0] =
+                            sum[i + 1][j][0] + sum[i][j + 1][0] - sum[i][j][0] + 1;
+                    sum[i + 1][j + 1][1] = 1;
+                } else if (grid[i][j] == 'Y') {
+                    sum[i + 1][j + 1][0] =
+                            sum[i + 1][j][0] + sum[i][j + 1][0] - sum[i][j][0] - 1;
+                    sum[i + 1][j + 1][1] = sum[i + 1][j][1] | sum[i][j + 1][1];
+                } else {
+                    sum[i + 1][j + 1][0] =
+                            sum[i + 1][j][0] + sum[i][j + 1][0] - sum[i][j][0];
+                    sum[i + 1][j + 1][1] = sum[i + 1][j][1] | sum[i][j + 1][1];
+                }
+                ans += (sum[i + 1][j + 1][0] == 0 && sum[i + 1][j + 1][1] == 1) ? 1 : 0;
+            }
+        }
+        return ans;
+    }
+}
