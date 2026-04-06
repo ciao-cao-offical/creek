f8b3103d852759399c03fc53a65b773000135fe8
ciao-cao-offical <843078365@qq.com>
2026-04-06 19:23:30 +0800

    feat(creek-java):LeetCode commit 2026/04/06

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2026/_04/RobotSim.java
@@ -0,0 +1,45 @@
+package cn.ccy.leetcode._2026._04;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/walking-robot-simulation/?envType=daily-question&envId=2026-04-06">874. 模拟行走机器人</a>
+ * @since 2026/4/6 19:19
+ */
+public class RobotSim {
+    public static void main(String[] args) {
+
+    }
+    
+    public int robotSim(int[] commands, int[][] obstacles) {
+        int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        int px = 0, py = 0, d = 1;
+        Set<Integer> set = new HashSet<Integer>();
+        for (int[] obstacle : obstacles) {
+            set.add(obstacle[0] * 60001 + obstacle[1]);
+        }
+        int res = 0;
+        for (int c : commands) {
+            if (c < 0) {
+                d += c == -1 ? 1 : -1;
+                d %= 4;
+                if (d < 0) {
+                    d += 4;
+                }
+            } else {
+                for (int i = 0; i < c; i++) {
+                    if (set.contains((px + dirs[d][0]) * 60001 + py + dirs[d][1])) {
+                        break;
+                    }
+                    px += dirs[d][0];
+                    py += dirs[d][1];
+                    res = Math.max(res, px * px + py * py);
+                }
+            }
+        }
+        return res;
+    }
+}
