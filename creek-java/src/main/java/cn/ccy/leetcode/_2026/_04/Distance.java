c60923fbe5a94a13d1da115166da0b5c1cf7df5f
ciao-cao-offical <843078365@qq.com>
2026-04-23 00:40:57 +0800

    feat(creek-java):LeetCode commit 2026/04/23

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2026/_04/Distance.java
@@ -0,0 +1,41 @@
+package cn.ccy.leetcode._2026._04;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/sum-of-distances/?envType=daily-question&envId=2026-04-23">2615. 等值距离和</a>
+ * @since 2026/4/23 00:38
+ */
+public class Distance {
+    public static void main(String[] args) {
+
+    }
+
+    public long[] distance(int[] nums) {
+        int n = nums.length;
+        Map<Integer, List<Integer>> groups = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            groups.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+        long[] res = new long[n];
+        for (List<Integer> group : groups.values()) {
+            long total = 0;
+            for (int idx : group) {
+                total += idx;
+            }
+            long prefixTotal = 0;
+            int sz = group.size();
+            for (int i = 0; i < sz; i++) {
+                int idx = group.get(i);
+                res[idx] = total - prefixTotal * 2 + (long) idx * (2 * i - sz);
+                prefixTotal += idx;
+            }
+        }
+        return res;
+    }
+}
