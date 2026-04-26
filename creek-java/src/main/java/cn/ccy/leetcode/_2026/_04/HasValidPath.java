30bbde8e1645ebbf08285c426e13185e54a0e40b
ciao-cao-offical <843078365@qq.com>
2026-04-27 00:03:22 +0800

    feat(creek-java):LeetCode commit 2026/04/27

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2026/_04/HasValidPath.java
@@ -0,0 +1,105 @@
+package cn.ccy.leetcode._2026._04;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/check-if-there-is-a-valid-path-in-a-grid/?envType=daily-question&envId=2026-04-27">1391. 检查网格中是否存在有效路径</a>
+ * @since 2026/4/27 00:01
+ */
+public class HasValidPath {
+    public static void main(String[] args) {
+
+    }
+
+    static class DisjointSet {
+        int[] f;
+
+        public DisjointSet(int m, int n) {
+            f = new int[m * n];
+            for (int i = 0; i < m * n; ++i) {
+                f[i] = i;
+            }
+        }
+
+        public int find(int x) {
+            return x == f[x] ? x : (f[x] = find(f[x]));
+        }
+
+        public void merge(int x, int y) {
+            f[find(x)] = find(y);
+        }
+    }
+
+    int[][] grid;
+    int m, n;
+    DisjointSet ds;
+
+    public boolean hasValidPath(int[][] grid) {
+        this.grid = grid;
+        m = grid.length;
+        n = grid[0].length;
+        ds = new DisjointSet(m, n);
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                handler(i, j);
+            }
+        }
+        return ds.find(getId(0, 0)) == ds.find(getId(m - 1, n - 1));
+    }
+
+    public int getId(int x, int y) {
+        return x * n + y;
+    }
+
+    public void detectL(int x, int y) {
+        if (y - 1 >= 0 && (grid[x][y - 1] == 4 || grid[x][y - 1] == 6 || grid[x][y - 1] == 1)) {
+            ds.merge(getId(x, y), getId(x, y - 1));
+        }
+    }
+
+    public void detectR(int x, int y) {
+        if (y + 1 < n && (grid[x][y + 1] == 3 || grid[x][y + 1] == 5 || grid[x][y + 1] == 1)) {
+            ds.merge(getId(x, y), getId(x, y + 1));
+        }
+    }
+
+    public void detectU(int x, int y) {
+        if (x - 1 >= 0 && (grid[x - 1][y] == 3 || grid[x - 1][y] == 4 || grid[x - 1][y] == 2)) {
+            ds.merge(getId(x, y), getId(x - 1, y));
+        }
+    }
+
+    public void detectD(int x, int y) {
+        if (x + 1 < m && (grid[x + 1][y] == 5 || grid[x + 1][y] == 6 || grid[x + 1][y] == 2)) {
+            ds.merge(getId(x, y), getId(x + 1, y));
+        }
+    }
+
+    public void handler(int x, int y) {
+        switch (grid[x][y]) {
+            case 1:
+                detectL(x, y);
+                detectR(x, y);
+                break;
+            case 2:
+                detectU(x, y);
+                detectD(x, y);
+                break;
+            case 3:
+                detectL(x, y);
+                detectD(x, y);
+                break;
+            case 4:
+                detectR(x, y);
+                detectD(x, y);
+                break;
+            case 5:
+                detectL(x, y);
+                detectU(x, y);
+                break;
+            case 6:
+                detectR(x, y);
+                detectU(x, y);
+        }
+    }
+}
