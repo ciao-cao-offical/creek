6a4e9bfa88f8da22325903cf172f1a8f20766bd0
ciao-cao-offical <843078365@qq.com>
2026-05-03 21:27:13 +0800

    feat(creek-java):LeetCode commit 2026/05/03

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2026/_05/RotateString.java
@@ -0,0 +1,33 @@
+package cn.ccy.leetcode._2026._05;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/rotate-string/?envType=daily-question&envId=2026-05-03">796. 旋转字符串</a>
+ * @since 2026/5/3 21:25
+ */
+public class RotateString {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean rotateString(String s, String goal) {
+        int m = s.length(), n = goal.length();
+        if (m != n) {
+            return false;
+        }
+        for (int i = 0; i < n; i++) {
+            boolean flag = true;
+            for (int j = 0; j < n; j++) {
+                if (s.charAt((i + j) % n) != goal.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
