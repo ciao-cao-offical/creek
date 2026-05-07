cdd24f36f917c7253450ee60ac8e9fce2db24d54
ciao-cao-offical <843078365@qq.com>
2026-05-08 00:36:11 +0800

    feat(creek-java):LeetCode commit 2026/05/08

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2026/_05/MinJumps.java
@@ -0,0 +1,73 @@
+package cn.ccy.leetcode._2026._05;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/minimum-jumps-to-reach-end-via-prime-teleportation/?envType=daily-question&envId=2026-05-08">3629. 通过质数传送到达终点的最少跳跃次数</a>
+ * @since 2026/5/8 00:34
+ */
+public class MinJumps {
+    public static void main(String[] args) {
+
+    }
+
+    private static final int MX = 1000001;
+    private static final List<Integer>[] factors = new ArrayList[MX];
+
+    static {
+        for (int i = 0; i < MX; i++) factors[i] = new ArrayList<>();
+        for (int i = 2; i < MX; i++) {
+            if (factors[i].isEmpty()) {
+                for (int j = i; j < MX; j += i) factors[j].add(i);
+            }
+        }
+    }
+
+    public int minJumps(int[] nums) {
+        int n = nums.length;
+        Map<Integer, List<Integer>> edges = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int a = nums[i];
+            if (factors[a].size() == 1) {
+                edges.computeIfAbsent(a, k -> new ArrayList<>()).add(i);
+            }
+        }
+        int res = 0;
+        boolean[] seen = new boolean[n];
+        seen[n - 1] = true;
+        List<Integer> q = new ArrayList<>();
+        q.add(n - 1);
+        while (true) {
+            List<Integer> q2 = new ArrayList<>();
+            for (int i : q) {
+                if (i == 0) return res;
+                if (i > 0 && !seen[i - 1]) {
+                    seen[i - 1] = true;
+                    q2.add(i - 1);
+                }
+                if (i < n - 1 && !seen[i + 1]) {
+                    seen[i + 1] = true;
+                    q2.add(i + 1);
+                }
+                for (int p : factors[nums[i]]) {
+                    if (edges.containsKey(p)) {
+                        for (int j : edges.get(p)) {
+                            if (!seen[j]) {
+                                seen[j] = true;
+                                q2.add(j);
+                            }
+                        }
+                        edges.get(p).clear();
+                    }
+                }
+            }
+            q = q2;
+            res++;
+        }
+    }
+}
