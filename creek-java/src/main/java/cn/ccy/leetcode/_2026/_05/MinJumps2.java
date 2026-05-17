fe9d38f1a8aeab1a752406c6b1495d8155cabbab
ciao-cao-offical <843078365@qq.com>
2026-05-18 00:05:21 +0800

    feat(creek-java):LeetCode commit 2026/05/18

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2026/_05/MinJumps2.java
@@ -0,0 +1,52 @@
+package cn.ccy.leetcode._2026._05;
+
+import java.util.*;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/jump-game-iv/?envType=daily-question&envId=2026-05-18">1345. 跳跃游戏 IV</a>
+ * @since 2026/5/18 00:03
+ * ☆☆☆☆☆ 2026cvd
+ */
+public class MinJumps2 {
+    public static void main(String[] args) {
+
+    }
+
+    public int minJumps(int[] arr) {
+        Map<Integer, List<Integer>> idxSameValue = new HashMap<Integer, List<Integer>>();
+        for (int i = 0; i < arr.length; i++) {
+            idxSameValue.putIfAbsent(arr[i], new ArrayList<Integer>());
+            idxSameValue.get(arr[i]).add(i);
+        }
+        Set<Integer> visitedIndex = new HashSet<Integer>();
+        Queue<int[]> queue = new ArrayDeque<int[]>();
+        queue.offer(new int[]{0, 0});
+        visitedIndex.add(0);
+        while (!queue.isEmpty()) {
+            int[] idxStep = queue.poll();
+            int idx = idxStep[0], step = idxStep[1];
+            if (idx == arr.length - 1) {
+                return step;
+            }
+            int v = arr[idx];
+            step++;
+            if (idxSameValue.containsKey(v)) {
+                for (int i : idxSameValue.get(v)) {
+                    if (visitedIndex.add(i)) {
+                        queue.offer(new int[]{i, step});
+                    }
+                }
+                idxSameValue.remove(v);
+            }
+            if (idx + 1 < arr.length && visitedIndex.add(idx + 1)) {
+                queue.offer(new int[]{idx + 1, step});
+            }
+            if (idx - 1 >= 0 && visitedIndex.add(idx - 1)) {
+                queue.offer(new int[]{idx - 1, step});
+            }
+        }
+        return -1;
+    }
+}
