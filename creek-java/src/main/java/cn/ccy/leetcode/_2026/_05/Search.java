624f3cf43913bc2dcad1ed1683f14499a7476c37
ciao-cao-offical <843078365@qq.com>
2026-05-22 00:07:37 +0800

    feat(creek-java):LeetCode commit 2026/05/22

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2026/_05/Search.java
@@ -0,0 +1,44 @@
+package cn.ccy.leetcode._2026._05;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/search-in-rotated-sorted-array/?envType=daily-question&envId=2026-05-22">33. 搜索旋转排序数组</a>
+ * @since 2026/5/22 00:06
+ */
+public class Search {
+    public static void main(String[] args) {
+
+    }
+
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return -1;
+        }
+        if (n == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
