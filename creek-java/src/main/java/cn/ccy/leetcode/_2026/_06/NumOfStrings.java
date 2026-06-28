c43fe965fc186254dd3cd6822c648883162d8336
ciao-cao-offical <843078365@qq.com>
2026-06-29 01:18:56 +0800

    feat(creek-java):LeetCode commit 2026/06/29

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2026/_06/NumOfStrings.java
@@ -0,0 +1,41 @@
+package cn.ccy.leetcode._2026._06;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/number-of-strings-that-appear-as-substrings-in-word/?envType=daily-question&envId=2026-06-29">1967. 作为子字符串出现在单词中的字符串数目</a>
+ * @since 2026/6/29 01:17
+ */
+public class NumOfStrings {
+    public static void main(String[] args) {
+
+    }
+
+    public int numOfStrings(String[] patterns, String word) {
+        int res = 0;
+        for (String pattern : patterns) {
+            if (check(pattern, word)) {
+                res++;
+            }
+        }
+        return res;
+    }
+
+    private boolean check(String pattern, String word) {
+        int m = pattern.length();
+        int n = word.length();
+        for (int i = 0; i + m <= n; i++) {
+            boolean flag = true;
+            for (int j = 0; j < m; j++) {
+                if (word.charAt(i + j) != pattern.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
