7d47da202f78f23a653cbc41e55baa8dc3e0b655
ciao-cao-offical <843078365@qq.com>
2026-07-03 01:34:31 +0800

    feat(creek-java):LeetCode commit 2026/07/03

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2026/_07/FindMaxPathScore.java
@@ -0,0 +1,95 @@
+package cn.ccy.leetcode._2026._07;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/network-recovery-pathways/?envType=daily-question&envId=2026-07-03">3620. 恢复网络路径</a>
+ * @since 2026/7/3 01:33
+ * ☆☆☆☆☆ 2026cvd
+ */
+public class FindMaxPathScore {
+    public static void main(String[] args) {
+
+    }
+
+    public int findMaxPathScore(int[][] edges, boolean[] online, long k) {
+        int n = online.length;
+        List<List<int[]>> g = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            g.add(new ArrayList<>());
+        }
+
+        int l = Integer.MAX_VALUE;
+        int r = 0;
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+            if (!online[u] || !online[v]) {
+                continue;
+            }
+            g.get(u).add(new int[]{v, w});
+            l = Math.min(l, w);
+            r = Math.max(r, w);
+        }
+
+        if (!check(g, l, k, n)) {
+            return -1;
+        }
+
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            if (check(g, mid, k, n)) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return r;
+    }
+
+    private boolean check(List<List<int[]>> g, int mid, long k, int n) {
+        long[] dis = new long[n];
+        Arrays.fill(dis, Long.MAX_VALUE);
+        PriorityQueue<long[]> pq = new PriorityQueue<>((a, b) ->
+                Long.compare(a[0], b[0])
+        );
+
+        dis[0] = 0;
+        pq.offer(new long[]{0, 0});
+
+        while (!pq.isEmpty()) {
+            long[] top = pq.poll();
+            long d = top[0];
+            int u = (int) top[1];
+
+            if (d > k) {
+                return false;
+            }
+            if (u == n - 1) {
+                return true;
+            }
+            if (d > dis[u]) {
+                continue;
+            }
+
+            for (int[] edge : g.get(u)) {
+                int v = edge[0];
+                int w = edge[1];
+                if (w < mid) {
+                    continue;
+                }
+                if (dis[v] > dis[u] + w) {
+                    dis[v] = dis[u] + w;
+                    pq.offer(new long[]{dis[v], v});
+                }
+            }
+        }
+        return false;
+    }
+}
