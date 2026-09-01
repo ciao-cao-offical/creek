0ca8f08f63ec3af8d7183c5b22effaf2cdbd71db
ciao-cao-offical <843078365@qq.com>
2026-09-02 00:16:08 +0800

    feat(creek-java):LeetCode commit 2026/09/02

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2026/_09/UniformArray.java
@@ -0,0 +1,17 @@
+package cn.ccy.leetcode._2026._09;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/construct-uniform-parity-array-i/?envType=daily-question&envId=2026-09-02">3875. 构造奇偶一致的数组 I</a>
+ * @since 2026/9/2 00:14
+ */
+public class UniformArray {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
