0a07d85f976997e0c615d5abda53d54bfa81ac0d
ciao-cao-offical <843078365@qq.com>
2026-09-07 00:33:28 +0800

    feat(creek-java):LeetCode commit 2026/09/07

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2026/_09/DistinctSubseqII.java
@@ -0,0 +1,42 @@
+package cn.ccy.leetcode._2026._09;
+
+import java.util.Arrays;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/distinct-subsequences-ii/?envType=daily-question&envId=2026-09-07">940. 不同的子序列 II</a>
+ * @since 2026/9/7 00:29
+ * ★★★★★ 2026cvd
+ */
+public class DistinctSubseqII {
+    public static void main(String[] args) {
+
+    }
+
+    public int distinctSubseqII(String s) {
+        final int MOD = 1000000007;
+        int[] last = new int[26];
+        Arrays.fill(last, -1);
+
+        int n = s.length();
+        int[] f = new int[n];
+        Arrays.fill(f, 1);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < 26; ++j) {
+                if (last[j] != -1) {
+                    f[i] = (f[i] + f[last[j]]) % MOD;
+                }
+            }
+            last[s.charAt(i) - 'a'] = i;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < 26; ++i) {
+            if (last[i] != -1) {
+                ans = (ans + f[last[i]]) % MOD;
+            }
+        }
+        return ans;
+    }
+}
