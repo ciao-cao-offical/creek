c76b90abe4bdc6c5d834e4e70e4b42b2e9df27a2
ciao-cao-offical <843078365@qq.com>
2026-09-13 19:23:51 +0800

    feat(creek-java):LeetCode commit 2026/09/13

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2026/_09/LargestOverlap.java
@@ -0,0 +1,31 @@
+package cn.ccy.leetcode._2026._09;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/image-overlap/?envType=daily-question&envId=2026-09-13">835. 图像重叠</a>
+ * @since 2026/9/13 19:22
+ */
+public class LargestOverlap {
+    public static void main(String[] args) {
+
+    }
+
+    public int largestOverlap(int[][] A, int[][] B) {
+        int N = A.length;
+        int[][] count = new int[2 * N + 1][2 * N + 1];
+        for (int i = 0; i < N; ++i)
+            for (int j = 0; j < N; ++j)
+                if (A[i][j] == 1)
+                    for (int i2 = 0; i2 < N; ++i2)
+                        for (int j2 = 0; j2 < N; ++j2)
+                            if (B[i2][j2] == 1)
+                                count[i - i2 + N][j - j2 + N] += 1;
+
+        int ans = 0;
+        for (int[] row : count)
+            for (int v : row)
+                ans = Math.max(ans, v);
+        return ans;
+    }
+}
