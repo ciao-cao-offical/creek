b17009e55e4f219c469351ff6726da15765e614e
ciao-cao-offical <843078365@qq.com>
2026-09-14 00:11:29 +0800

    feat(creek-java):LeetCode commit 2026/09/14

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2026/_09/IsRectangleOverlap.java
@@ -0,0 +1,23 @@
+package cn.ccy.leetcode._2026._09;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/rectangle-overlap/?envType=daily-question&envId=2026-09-14">836. 矩形重叠</a>
+ * @since 2026/9/14 00:10
+ */
+public class IsRectangleOverlap {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        if (rec1[0] == rec1[2] || rec1[1] == rec1[3] || rec2[0] == rec2[2] || rec2[1] == rec2[3]) {
+            return false;
+        }
+        return !(rec1[2] <= rec2[0] ||   // left
+                rec1[3] <= rec2[1] ||   // bottom
+                rec1[0] >= rec2[2] ||   // right
+                rec1[1] >= rec2[3]);    // top
+    }
+}
