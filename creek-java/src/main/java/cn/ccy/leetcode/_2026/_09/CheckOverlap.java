80276cbc87c3e4419a474654ffe21467305f10bb
ciao-cao-offical <843078365@qq.com>
2026-09-19 01:22:28 +0800

    feat(creek-java):LeetCode commit 2026/09/19

---
+++ creek-java/src/main/java/cn/ccy/leetcode/_2026/_09/CheckOverlap.java
@@ -0,0 +1,58 @@
+package cn.ccy.leetcode._2026._09;
+
+/**
+ * @author caochengyin
+ * @version v 1.0.0
+ * @see <a href="https://leetcode.cn/problems/circle-and-rectangle-overlapping/?envType=daily-question&envId=2026-09-19">1401. 圆和矩形是否有重叠</a>
+ * @since 2026/9/19 01:20
+ */
+public class CheckOverlap {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        /* 圆心在矩形内部 */
+        if (x1 <= xCenter && xCenter <= x2 && y1 <= yCenter && yCenter <= y2) {
+            return true;
+        }
+        /* 圆心在矩形上部 */
+        if (x1 <= xCenter && xCenter <= x2 && y2 <= yCenter && yCenter <= y2 + radius) {
+            return true;
+        }
+        /* 圆心在矩形下部 */
+        if (x1 <= xCenter && xCenter <= x2 && y1 - radius <= yCenter && yCenter <= y1) {
+            return true;
+        }
+        /* 圆心在矩形左部 */
+        if (x1 - radius <= xCenter && xCenter <= x1 && y1 <= yCenter && yCenter <= y2) {
+            return true;
+        }
+        /* 圆心在矩形右部 */
+        if (x2 <= xCenter && xCenter <= x2 + radius && y1 <= yCenter && yCenter <= y2) {
+            return true;
+        }
+        /* 矩形左上角 */
+        if (distance(xCenter, yCenter, x1, y2) <= radius * radius) {
+            return true;
+        }
+        /* 矩形左下角 */
+        if (distance(xCenter, yCenter, x1, y1) <= radius * radius) {
+            return true;
+        }
+        /* 矩形右上角 */
+        if (distance(xCenter, yCenter, x2, y2) <= radius * radius) {
+            return true;
+        }
+        /* 矩形右下角 */
+        if (distance(xCenter, yCenter, x1, y2) <= radius * radius) {
+            return true;
+        }
+        /* 无交点 */
+        return false;
+    }
+
+    public long distance(int ux, int uy, int vx, int vy) {
+        return (long) Math.pow(ux - vx, 2) + (long) Math.pow(uy - vy, 2);
+    }
+}
